d48db84cadaa44e1d689199b1f2ea8e74c4b2225
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2021-01-07 11:26:59 +0000

    added pbzip2 payload

--- bundle.py
+++ bundle.py
@@ -11,17 +11,29 @@ pligin_dirname = os.path.dirname(os.path.abspath(__file__))
 plugin_file_name = os.path.basename(pligin_dirname) + '.py'
 python_source = os.path.join(pligin_dirname, plugin_file_name)
 bundle_folder = os.path.basename(pligin_dirname) + '.bundle'
-bundle_code = 'unpacker.py'
+bundle_code = 'flameTimewarpML.py'
 
 if not os.path.isdir(bundle_folder):
     print('no folder named %s' % bundle_folder)
     sys.exit()
 
+'''
 print ('creating %s' % bundle_folder + '.tar.bz2\n---')
-cmd = 'tar cjvf ' + bundle_folder + '.tar.bz2 ' + bundle_folder
+cmd = 'tar cvf ' + bundle_folder + '.tar ' + bundle_folder
 os.system(cmd)
+cmd = 'pbzip2 -v ' + bundle_folder + '.tar'
+os.system(cmd)
+cmd = 'mv ' + bundle_folder + '.tar.bz2 flameTimewarpML.package/'
+os.system(cmd)
+'''
 
+'''
 f = open(bundle_folder + '.tar.bz2', 'rb')
+bunle_data = f.read()
+f.close()
+'''
+
+f = open('/usr/bin/pbzip2', 'rb')
 bunle_data = f.read()
 f.close()
 
@@ -33,13 +45,16 @@ f.close()
 
 bundled_code =bundle_code.replace('REPLACEME', encoded_bundle_data)
 
-with open('tmp.py', 'w') as tempfile:
+with open('flameTimewarpML.package/flameTimewarpML.py', 'w') as tempfile:
     with open(python_source, 'r') as src:
         tempfile.write(src.read())
         tempfile.write(bundled_code)
 
-cmd = 'mv tmp.py /opt/Autodesk/shared/python/' + plugin_file_name
-os.system(cmd)
 
-print ('---\nremoving %s' % bundle_folder + '.tar.bz2')
-os.remove(bundle_folder + '.tar.bz2')
+
+
+# cmd = 'mv tmp.py /opt/Autodesk/shared/python/' + plugin_file_name
+# os.system(cmd)
+
+# print ('---\nremoving %s' % bundle_folder + '.tar.bz2')
+# os.remove(bundle_folder + '.tar.bz2')
