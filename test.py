1f762ba8bb964c0b466a9837674c533e9937286f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2021-01-12 02:43:45 +0000

    linxu cmd line tests

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import subprocess
 
@@ -5,5 +6,10 @@ import subprocess
 #    p = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, universal_newlines=True)
 #    p.communicate(cmd)
 
-cmd = """tell application "Terminal" to activate do script "/bin/bash -c 'eval " & quote & "$(~/Documents/flameTimewarpML/miniconda3/bin/conda shell.bash hook)" & quote & "; conda activate; sleep 5'; exit"  """
-subprocess.Popen(['osascript', '-e', cmd])
+# cmd = """tell application "Terminal" to activate do script "/bin/bash -c 'eval " & quote & "$(~/Documents/flameTimewarpML/miniconda3/bin/conda shell.bash hook)" & quote & "; conda activate; sleep 5'; exit"  """
+# subprocess.Popen(['osascript', '-e', cmd])
+logfile_path = '/var/tmp/flameTimewarpML_install.log'
+log_cmd = "konsole -e /bin/bash -c 'tail -f " + os.path.abspath(logfile_path) +"'"
+print (log_cmd)
+os.system(log_cmd)
+# subprocess.Popen(['konsole', '-e', log_cmd])
