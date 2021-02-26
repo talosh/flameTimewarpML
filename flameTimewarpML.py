fc15672d5ab16c112d90ab5236028b7a1981f56d
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2021-02-26 08:55:49 +0000

    removed clamping ov values under and over 1

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -22,7 +22,7 @@ FLAMETWML_MINICONDA_LINUX = ''
 menu_group_name = 'Timewarp ML'
 DEBUG = False
 
-__version__ = 'v0.4.1.beta.006'
+__version__ = 'v0.4.1.beta.007'
 
 
 if os.getenv('FLAMETWML_BUNDLE') and not FLAMETWML_BUNDLE_MAC:
