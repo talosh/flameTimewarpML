1daef16147817566d0a37bfe2f7e98d27ca97c80
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-06-22 10:44:45 +0100

    update bundle.py - updated to flame2023.0.1

--- bundle.py
+++ bundle.py
@@ -13,7 +13,7 @@ parser.add_argument('--copy', dest='copy', action='store_true', help='copy to /o
 parser.add_argument('--run', dest='run', action='store_true', help='run flame')
 args = parser.parse_args()
 
-flame_cmd = '/opt/Autodesk/flame_2022.1/bin/startApplication'
+flame_cmd = '/opt/Autodesk/flame_2023.0.1/bin/startApplication'
 plugin_dirname = os.path.dirname(os.path.abspath(__file__))
 plugin_file_name = os.path.basename(plugin_dirname) + '.py'
 python_source = os.path.join(plugin_dirname, plugin_file_name)
