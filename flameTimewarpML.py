eb7afe5c5c9925c2f33cd0eb09e6d31b3893c1d8
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-09-23 11:49:38 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -177,13 +177,11 @@ class flameAppFramework(object):
             'site-packages'
         )
 
-        '''
         if not self.check_bundle_id():
             threading.Thread(
                 target=self.unpack_bundle,
                 args=(os.path.dirname(self.site_packages_folder), )
             ).start()
-        '''
 
     def log(self, message):
         try:
