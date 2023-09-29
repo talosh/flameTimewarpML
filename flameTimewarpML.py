322c71259f55100ebe028026876e7d4f170126b6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-09-29 16:16:53 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -1557,7 +1557,7 @@ class flameTimewarpML(flameMenuApp):
             self.message_queue.put({'type': 'info', 'message': 'Creating destination library...'})
             self.processEvents()
 
-            pprint (self.selection.attributes)
+            pprint (self.selection.duration)
 
             self.destination_node_id = self.parent_app.create_destination_node(
                 self.selection,
