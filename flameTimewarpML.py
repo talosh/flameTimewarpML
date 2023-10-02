c95398c4bdae40d04aae89ec5b2a005ccf86a632
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-02 13:01:29 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -1284,7 +1284,7 @@ class flameTimewarpML(flameMenuApp):
 
                 # End frame label
                 self.end_frame_label = QtWidgets.QLabel('100', Progress)
-                self.end_frame_label.setFixedWidth(80)
+                self.end_frame_label.setFixedWidth(50)
                 self.end_frame_label.setContentsMargins(10, 0, 10, 0)
                 self.end_frame_label.setStyleSheet(
                     'QLabel {color: rgb(154, 154, 154); background-color: #292929; border: 1px solid #474747;}'
