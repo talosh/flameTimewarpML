5b6e553ce6293e0dc1042c1628f4d535139ba861
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-02 17:30:04 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -5488,7 +5488,6 @@ class flameTimewarpML(flameMenuApp):
                 flow0 = self.block0(x)
                 F1 = flow0
                 F1_large = F.interpolate(F1, scale_factor=2.0, mode="bilinear", align_corners=False, recompute_scale_factor=False) * 2.0
-                print (f'F1_shape {F1_large[:, :2, :h, :w].shape}')
                 display_flow = F.interpolate(F1_large[:, :2, :h, :w], scale_factor=0.25, mode='nearest')
                 display_flow = display_flow.cpu().detach().numpy()
                 self.progress.update_optical_flow(
@@ -5499,7 +5498,7 @@ class flameTimewarpML(flameMenuApp):
 
                 warped_img0 = warp(x[:, :3], F1_large[:, :2])
                 warped_img1 = warp(x[:, 3:], F1_large[:, 2:4])
-                display_warp = F.interpolate((warped_img0 + warped_img1) / 2, scale_factor=0.25, mode='nearest')
+                display_warp = F.interpolate((warped_img0[:, : :h, :w] + warped_img1[:, : :h, :w]) / 2, scale_factor=0.25, mode='nearest')
                 display_warp = display_warp[0].cpu().detach().numpy().transpose(1, 2, 0)
                 display_warp = np.flip(display_warp, axis=2).copy()
                 self.progress.update_interface_image(
@@ -5512,52 +5511,18 @@ class flameTimewarpML(flameMenuApp):
                 F2 = (flow0 + flow1)
                 F2_large = F.interpolate(F2, scale_factor=2.0, mode="bilinear", align_corners=False, recompute_scale_factor=False) * 2.0
 
-                '''
-                display_flow = F.interpolate(F2_large, scale_factor=0.25, mode='nearest')
-                display_flow = display_flow[:, :2].cpu().detach().numpy()
-                self.progress.update_optical_flow(
-                    display_flow,
-                    self.progress.ui.flow2_label,
-                    text = f'Flow pass 2 of 4'
-                    )
-                '''
+
 
                 warped_img0 = warp(x[:, :3], F2_large[:, :2])
                 warped_img1 = warp(x[:, 3:], F2_large[:, 2:4])
-                display_warp = F.interpolate((warped_img0 + warped_img1) / 2, scale_factor=0.25, mode='nearest')
-                display_warp = display_warp[0].cpu().detach().numpy().transpose(1, 2, 0)[:h, :w]
-                display_warp = np.flip(display_warp, axis=2).copy()
-                self.progress.update_interface_image(
-                    display_warp,
-                    self.progress.ui.flow3_label,
-                    text = f'Warp'
-                    )
 
 
                 flow2 = self.block2(torch.cat((warped_img0, warped_img1, F2_large), 1))
                 F3 = (flow0 + flow1 + flow2)
                 F3_large = F.interpolate(F3, scale_factor=2.0, mode="bilinear", align_corners=False, recompute_scale_factor=False) * 2.0
 
-                '''
-                display_flow = F.interpolate(F3_large, scale_factor=0.25, mode='nearest')
-                display_flow = display_flow[:, :2].cpu().detach().numpy()
-                self.progress.update_optical_flow(
-                    display_flow,
-                    self.progress.ui.flow2_label,
-                    text = f'Flow pass 3 of 4'
-                    )
-                '''
-
                 warped_img0 = warp(x[:, :3], F3_large[:, :2])
                 warped_img1 = warp(x[:, 3:], F3_large[:, 2:4])
-                display_warp = F.interpolate((warped_img0 + warped_img1) / 2, scale_factor=0.25, mode='nearest')
-                display_warp = display_warp[0].cpu().detach().numpy().transpose(1, 2, 0)[:h, :w]
-                display_warp = np.flip(display_warp, axis=2).copy()
-                self.progress.update_interface_image(
-                    display_warp,
-                    self.progress.ui.flow3_label,
-                    text = f'Warp'
-                    )
 
                 flow3 = self.block3(torch.cat((warped_img0, warped_img1, F3_large), 1))
                 F4 = (flow0 + flow1 + flow2 + flow3)
