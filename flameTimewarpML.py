ec5c3f2dd04f722dfffc96eb453a734879edcfab
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-02 15:36:46 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -2194,9 +2194,7 @@ class flameTimewarpML(flameMenuApp):
                         col0 = tmp[k0] / 255.0
                         col1 = tmp[k1] / 255.0
                         col = (1-f)*col0 + f*col1
-                        idx = (rad <= 1)
-                        col[idx]  = rad[idx] * col[idx]
-                        col[~idx] = col[~idx] * 0.75   # out of range
+                        col  = rad * col
                         # Note the 2-i => BGR instead of RGB
                         ch_idx = 2-i if convert_to_bgr else i
                         flow_image[:,:,ch_idx] = np.floor(255 * col)
