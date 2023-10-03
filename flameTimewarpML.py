c8cf1976bcebf97950968ee82dd42977327d5c38
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-03 13:32:34 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -2024,8 +2024,8 @@ class flameTimewarpML(flameMenuApp):
                     values_10bit[::3] = (byte_array >> 22) & 0x3FF
                     values_10bit[1::3] = (byte_array >> 12) & 0x3FF
                     values_10bit[2::3] = (byte_array >> 2) & 0x3FF
-                    values_16bit = (values_10bit.astype(np.float32) // 256) * 65535
-                    values_16bit = values_10bit.astype(np.uint16)
+                    # values_16bit = (values_10bit.astype(np.float32) // 256) * 65535
+                    values_16bit = values_10bit * 2
 
                     # Extract the three 10-bit values from each 4-byte sequence
                     '''
