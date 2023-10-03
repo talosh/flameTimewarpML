e040d28c5c55b832085f50625cb4b6788e512b5a
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-03 15:47:42 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -1718,9 +1718,8 @@ class flameTimewarpML(flameMenuApp):
         
         def restore_normalized_values(self, image_array):
             import torch
-            epsilon = torch.tensor(1.40129846e-28, dtype=torch.float32).to(image_array.device)
-            image_array = torch.clamp(image_array, min=epsilon, max=1.0-epsilon)
-            return (torch.arctanh(torch.clamp((image_array * 2) - 1, -1.0, 1.0)) + 1.0) / 2.0
+            epsilon = torch.tensor(1.40129846e-18, dtype=torch.float32).to(image_array.device)
+            return (torch.arctanh(torch.clamp((image_array * 2) - 1, -1.0 + epsilon, 1.0 - epsilon)) + 1.0) / 2.0
 
         def _process_current_frame(self, single_frame=False):
             import numpy as np
