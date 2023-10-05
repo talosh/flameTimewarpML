5ee8ef2950ac759f1d99bb4da180848fac061b04
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-05 13:30:06 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -1616,6 +1616,11 @@ class flameTimewarpML(flameMenuApp):
             self.min_frame = min(self.frames_map.keys())
             self.max_frame = max(self.frames_map.keys())
             self.set_current_frame(self.min_frame)
+            self.message_queue.put(
+                {'type': 'setText',
+                'widget': 'cur_frame_label',
+                'text': str(self.min_frame)}
+            )
 
             self.message_queue.put(
                 {'type': 'setText',
