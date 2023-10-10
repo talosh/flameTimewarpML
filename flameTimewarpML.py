c3a305e0a04071f1ce418a6b146b5bce05c8e95d
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-10-10 17:04:50 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -6502,7 +6502,7 @@ class flameTimewarpML(flameMenuApp):
             print ('trying RAFT')
             img0_raft = F.interpolate(img0*2 - 1, scale_factor=0.5, mode="bilinear", align_corners=False)
             img1_raft = F.interpolate(img1*2 - 1, scale_factor=0.5, mode="bilinear", align_corners=False)
-            raft_flow_fwd = self.raft(img0_raft, img1_raft)
+            raft_flow_fwd = self.raft(img0_raft, img1_raft) * -1
             raft_flow_bkw = self.raft(img1_raft, img0_raft)
             raft_flow = torch.cat((raft_flow_fwd, raft_flow_bkw), 1)
             # print (f'flow shape: {flow.shape}, raft_flow shape: {raft_flow.shape}')
