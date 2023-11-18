103f57be26629066220f4387635d47a271547549
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-18 17:20:44 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -6355,6 +6355,7 @@ class flameTimewarpML(flameMenuApp):
 
             def forward(self, x, timestep=0.5, scale_list=[8, 4, 2, 1], training=False, fastmode=True, ensemble=True):
                 print (f'x shape: {x.shape}')
+                print (f'x[:, :1] shape {x[:, :1].clone().shape}')
                 if training == False:
                     channel = x.shape[1] // 2
                     img0 = x[:, :channel]
