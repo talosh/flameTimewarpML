b3472391bf805239f5b221f194cbf4864f9efb3c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-18 18:06:45 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -6373,18 +6373,18 @@ class flameTimewarpML(flameMenuApp):
                 mask = None
                 block = [self.block0, self.block1, self.block2, self.block3]
 
-                self.empty_torch_cache()
+                # self.empty_torch_cache()
                 info_text = self.progress.ui.info_label.text()
 
                 for i in range(4):
                     self.progress.info(f'{info_text} - flow iteration {i} of 4')
                     if flow is None:
                         flow, mask = block[i](torch.cat((img0[:, :3], img1[:, :3], f0, f1, timestep), 1), None, scale=scale_list[i])
-                        self.empty_torch_cache()
+                        # self.empty_torch_cache()
 
                         if ensemble:
                             f_, m_ = block[i](torch.cat((img1[:, :3], img0[:, :3], f1, f0, 1-timestep), 1), None, scale=scale_list[i])
-                            self.empty_torch_cache()
+                            # self.empty_torch_cache()
 
                             flow = (flow + torch.cat((f_[:, 2:4], f_[:, :2]), 1)) / 2
                             del f_
@@ -6395,11 +6395,11 @@ class flameTimewarpML(flameMenuApp):
                         wf0 = warp(f0, flow[:, :2])
                         wf1 = warp(f1, flow[:, 2:4])
                         fd, m0 = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], wf0, wf1, timestep, mask), 1), flow, scale=scale_list[i])
-                        self.empty_torch_cache()
+                        # self.empty_torch_cache()
 
                         if ensemble:
                             f_, m_ = block[i](torch.cat((warped_img1[:, :3], warped_img0[:, :3], wf1, wf0, 1-timestep, -mask), 1), torch.cat((flow[:, 2:4], flow[:, :2]), 1), scale=scale_list[i])
-                            self.empty_torch_cache()
+                            # self.empty_torch_cache()
 
                             fd = (fd + torch.cat((f_[:, 2:4], f_[:, :2]), 1)) / 2
                             del f_
@@ -6429,7 +6429,7 @@ class flameTimewarpML(flameMenuApp):
                         )
 
 
-                    self.empty_torch_cache()
+                    # self.empty_torch_cache()
                     # mask_list.append(mask)
                     # flow_list.append(flow)
                     warped_img0 = warp(img0, flow[:, :2])
