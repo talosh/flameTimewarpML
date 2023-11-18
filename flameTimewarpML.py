2fcc7c5a7c43b6367a15dae07658ff3a323eef2d
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-18 21:47:51 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -6377,7 +6377,7 @@ class flameTimewarpML(flameMenuApp):
                 block = [self.block0, self.block1, self.block2, self.block3]
 
 
-                '''
+                # '''
                 raft_img0 = F.interpolate(img0 * 2 - 1, scale_factor = 1 / 4, mode="bilinear", align_corners=False)
                 raft_img1 = F.interpolate(img1 * 2 - 1, scale_factor = 1 / 4, mode="bilinear", align_corners=False)
                 current_device = torch.device(img0.device)
@@ -6420,8 +6420,9 @@ class flameTimewarpML(flameMenuApp):
                 warped_img1 = warp(img1, flow[:, 2:4])
                 mask = img0[:, :1].clone() * 0
                 block = [self.block1, self.block1, self.block2, self.block3]
+                return warped_img0
 
-                '''
+                # '''
 
                 # self.empty_torch_cache()
 
@@ -6481,8 +6482,8 @@ class flameTimewarpML(flameMenuApp):
                     warped_img1 = warp(img1, flow[:, 2:4])
 
                 mask = torch.sigmoid(mask)
-                # merged = (warped_img0 * mask + warped_img1 * (1 - mask))
-                merged = warped_img0
+                merged = (warped_img0 * mask + warped_img1 * (1 - mask))
+                # merged = warped_img0
 
                 if not fastmode:
                     print('contextnet is removed')
