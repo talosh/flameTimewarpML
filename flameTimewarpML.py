ec604902d7ff52b8cfd8f4ff72ce546073fc9146
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-18 16:25:05 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -5990,12 +5990,16 @@ class flameTimewarpML(flameMenuApp):
         import torch.nn as nn
         import torch.nn.functional as F
 
-        torch.set_grad_enabled(False)
         if sys.platform == 'darwin':
             device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
             # device = torch.device('cpu')
         else:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        if self.modes[self.current_mode].startswith('CPU'):
+            self.torch_device = torch.device('cpu')
+
+        device = self.torch_device
 
         img0 = img0.flip(-1).contiguous()
         img1 = img1.flip(-1).contiguous()
@@ -6220,7 +6224,6 @@ class flameTimewarpML(flameMenuApp):
         flow, mask, merged = flownet(imgs, timestep, scale_list)
 
         res_img = merged[3][0]
-        print (f'res img shape: {res_img.shape}')
         res_img = res_img.permute(1, 2, 0)[:h, :w]
         res_img = res_img.flip(-1)
 
@@ -6239,6 +6242,11 @@ class flameTimewarpML(flameMenuApp):
         else:
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+        if self.modes[self.current_mode].startswith('CPU'):
+            self.torch_device = torch.device('cpu')
+
+        device = self.torch_device
+
         torch.set_grad_enabled(False)
 
         img0 = img0.flip(-1).contiguous()
@@ -6254,6 +6262,190 @@ class flameTimewarpML(flameMenuApp):
         img0 = F.pad(img0, padding)
         img1 = F.pad(img1, padding)
 
+        def warp(tenInput, tenFlow):
+            backwarp_tenGrid = {}
+
+            k = (str(tenFlow.device), str(tenFlow.size()))
+            if k not in backwarp_tenGrid:
+                tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3], device=device).view(
+                    1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
+                tenVertical = torch.linspace(-1.0, 1.0, tenFlow.shape[2], device=device).view(
+                    1, 1, tenFlow.shape[2], 1).expand(tenFlow.shape[0], -1, -1, tenFlow.shape[3])
+                backwarp_tenGrid[k] = torch.cat(
+                    [tenHorizontal, tenVertical], 1).to(device)
+
+            tenFlow = torch.cat([tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0),
+                                tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0)], 1)
+
+            g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
+            return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
+
+        def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
+            return nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                        padding=padding, dilation=dilation, bias=True),        
+                nn.LeakyReLU(0.2, True)
+            )
+
+        class ResConv(nn.Module):
+            def __init__(self, c, dilation=1):
+                super(ResConv, self).__init__()
+                self.conv = nn.Conv2d(c, c, 3, 1, dilation, dilation=dilation, groups=1\
+        )
+                self.beta = nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
+                self.relu = nn.LeakyReLU(0.2, True)
+
+            def forward(self, x):
+                return self.relu(self.conv(x) * self.beta + x)
+
+        class IFBlock(nn.Module):
+            def __init__(self, in_planes, c=64):
+                super(IFBlock, self).__init__()
+                self.conv0 = nn.Sequential(
+                    conv(in_planes, c//2, 3, 2, 1),
+                    conv(c//2, c, 3, 2, 1),
+                    )
+                self.convblock = nn.Sequential(
+                    ResConv(c),
+                    ResConv(c),
+                    ResConv(c),
+                    ResConv(c),
+                    ResConv(c),
+                    ResConv(c),
+                    ResConv(c),
+                    ResConv(c),
+                )
+                self.lastconv = nn.Sequential(
+                    nn.ConvTranspose2d(c, 4*6, 4, 2, 1),
+                    nn.PixelShuffle(2)
+                )
+
+            def forward(self, x, flow=None, scale=1):
+                x = F.interpolate(x, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
+                if flow is not None:
+                    flow = F.interpolate(flow, scale_factor= 1. / scale, mode="bilinear", align_corners=False) * 1. / scale
+                    x = torch.cat((x, flow), 1)
+                feat = self.conv0(x)
+                feat = self.convblock(feat)
+                tmp = self.lastconv(feat)
+                tmp = F.interpolate(tmp, scale_factor=scale, mode="bilinear", align_corners=False)
+                flow = tmp[:, :4] * scale
+                mask = tmp[:, 4:5]
+                return flow, mask
+                
+        class IFNet(nn.Module):
+            def __init__(self):
+                super(IFNet, self).__init__()
+                self.block0 = IFBlock(7+16, c=192)
+                self.block1 = IFBlock(8+4+16, c=128)
+                self.block2 = IFBlock(8+4+16, c=96)
+                self.block3 = IFBlock(8+4+16, c=64)
+                self.encode = nn.Sequential(
+                    nn.Conv2d(3, 32, 3, 2, 1),
+                    nn.LeakyReLU(0.2, True),
+                    nn.Conv2d(32, 32, 3, 1, 1),
+                    nn.LeakyReLU(0.2, True),
+                    nn.Conv2d(32, 32, 3, 1, 1),
+                    nn.LeakyReLU(0.2, True),
+                    nn.ConvTranspose2d(32, 8, 4, 2, 1)
+                )
+                # self.contextnet = Contextnet()
+                # self.unet = Unet()
+
+            def forward(self, x, timestep=0.5, scale_list=[8, 4, 2, 1], training=False, fastmode=True, ensemble=False):
+                if training == False:
+                    channel = x.shape[1] // 2
+                    img0 = x[:, :channel]
+                    img1 = x[:, channel:]
+                if not torch.is_tensor(timestep):
+                    timestep = (x[:, :1].clone() * 0 + 1) * timestep
+                else:
+                    timestep = timestep.repeat(1, 1, img0.shape[2], img0.shape[3])
+                f0 = self.encode(img0[:, :3])
+                f1 = self.encode(img1[:, :3])
+                flow_list = []
+                merged = []
+                mask_list = []
+                warped_img0 = img0
+                warped_img1 = img1
+                flow = None
+                mask = None
+                loss_cons = 0
+                block = [self.block0, self.block1, self.block2, self.block3]
+                for i in range(4):
+                    if flow is None:
+                        flow, mask = block[i](torch.cat((img0[:, :3], img1[:, :3], f0, f1, timestep), 1), None, scale=scale_list[i])
+                        if ensemble:
+                            f_, m_ = block[i](torch.cat((img1[:, :3], img0[:, :3], f1, f0, 1-timestep), 1), None, scale=scale_list[i])
+                            flow = (flow + torch.cat((f_[:, 2:4], f_[:, :2]), 1)) / 2
+                            mask = (mask + (-m_)) / 2
+                    else:
+                        wf0 = warp(f0, flow[:, :2])
+                        wf1 = warp(f1, flow[:, 2:4])
+                        fd, m0 = block[i](torch.cat((warped_img0[:, :3], warped_img1[:, :3], wf0, wf1, timestep, mask), 1), flow, scale=scale_list[i])
+                        if ensemble:
+                            f_, m_ = block[i](torch.cat((warped_img1[:, :3], warped_img0[:, :3], wf1, wf0, 1-timestep, -mask), 1), torch.cat((flow[:, 2:4], flow[:, :2]), 1), scale=scale_list[i])
+                            fd = (fd + torch.cat((f_[:, 2:4], f_[:, :2]), 1)) / 2
+                            mask = (m0 + (-m_)) / 2
+                        else:
+                            mask = m0
+                        flow = flow + fd
+                    mask_list.append(mask)
+                    flow_list.append(flow)
+                    warped_img0 = warp(img0, flow[:, :2])
+                    warped_img1 = warp(img1, flow[:, 2:4])
+                    merged.append((warped_img0, warped_img1))
+                mask = torch.sigmoid(mask)
+                merged[3] = (warped_img0 * mask + warped_img1 * (1 - mask))
+                if not fastmode:
+                    print('contextnet is removed')
+                    '''
+                    c0 = self.contextnet(img0, flow[:, :2])
+                    c1 = self.contextnet(img1, flow[:, 2:4])
+                    tmp = self.unet(img0, img1, warped_img0, warped_img1, mask, flow, c0, c1)
+                    res = tmp[:, :3] * 2 - 1
+                    merged[3] = torch.clamp(merged[3] + res, 0, 1)
+                    '''
+                return flow_list, mask_list[3], merged
+
+        def convert(param):
+            return {
+                k.replace("module.", ""): v
+                for k, v in param.items()
+                if "module." in k
+            }
+
+
+        flownet = IFNet()
+        flownet.to(device)
+        flownet.eval()
+
+        model_path = os.path.join(
+            self.trained_models_path,
+            'v4.7.model',
+            'flownet.pkl'
+        )
+
+        flownet.load_state_dict(convert(torch.load(model_path)), False)
+        scale = self.flow_scale
+        timestep = ratio
+
+        imgs = torch.cat((img0, img1), 1)
+        scale_list = [8/scale, 4/scale, 2/scale, 1/scale]
+
+        img0_raft = F.interpolate(img0*2 - 1, scale_factor= 1 / 4, mode="bilinear", align_corners=False)
+        img1_raft = F.interpolate(img1*2 - 1, scale_factor= 1 / 4, mode="bilinear", align_corners=False)
+        raft_flow_fwd = self.raft(img0_raft, img1_raft) * - (timestep)
+        raft_flow_bkw = self.raft(img1_raft, img0_raft) * - (1 - timestep)
+
+        flow, mask, merged = flownet(imgs, timestep, scale_list)
+
+        res_img = merged[3][0]
+        res_img = res_img.permute(1, 2, 0)[:h, :w]
+        res_img = res_img.flip(-1)
+
+        return res_img
+
     def flownet24(self, img0, img1, ratio, model_path):
         import torch
         import torch.nn as nn
@@ -6265,6 +6457,7 @@ class flameTimewarpML(flameMenuApp):
             self.torch_device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
         else:
             self.torch_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            
         if self.modes[self.current_mode].startswith('CPU'):
             self.torch_device = torch.device('cpu')
 
