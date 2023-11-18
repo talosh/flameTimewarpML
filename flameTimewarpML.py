38ed0c099fb457d96f9d3cb6be827c5408b1a740
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-18 14:13:32 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -6439,8 +6439,8 @@ class flameTimewarpML(flameMenuApp):
                 info_text = self.progress.ui.info_label.text()
 
                 if (self.progress.parent_app.current_mode == 3) or (self.progress.parent_app.current_mode == 6):
-                    raft_img0 = F.interpolate(x[:, :3]*2 - 1, scale_factor= 1 / ( 8 * flow_scale ) , mode="bilinear", align_corners=False)
-                    raft_img1 = F.interpolate(x[:, 3:]*2 - 1, scale_factor= 1 / ( 8 * flow_scale ), mode="bilinear", align_corners=False)
+                    raft_img0 = F.interpolate(x[:, :3]*2 - 1, scale_factor = 1 / ( 4 * flow_scale ) , mode="bilinear", align_corners=False)
+                    raft_img1 = F.interpolate(x[:, 3:]*2 - 1, scale_factor = 1 / ( 4 * flow_scale ), mode="bilinear", align_corners=False)
 
                     current_device = torch.device(img0.device)
                     try:
@@ -6477,8 +6477,8 @@ class flameTimewarpML(flameMenuApp):
                         )
 
                     FR = torch.cat((raft_flow_f, raft_flow_b), 1)
-                    FR_large = F.interpolate(FR, scale_factor=8 * flow_scale, mode="bilinear", align_corners=False, recompute_scale_factor=False) * ( 8 * flow_scale )
-                    FR = F.interpolate(FR_large, scale_factor=1 / 2, mode="bilinear", align_corners=False, recompute_scale_factor=False) * 0.5
+                    FR_large = F.interpolate(FR, scale_factor = 4 * flow_scale, mode="bilinear", align_corners=False, recompute_scale_factor=False) * ( 8 * flow_scale )
+                    FR = F.interpolate(FR_large, scale_factor = 1 / 2, mode="bilinear", align_corners=False, recompute_scale_factor=False) * 0.5
                     
                     warped_img0 = warp(x[:, :3], FR_large[:, :2])
                     warped_img1 = warp(x[:, 3:], FR_large[:, 2:4])
