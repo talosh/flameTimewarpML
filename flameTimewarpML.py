dfbf1b96d54b70966d4d6186941071afb6144282
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-19 10:30:37 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -6440,7 +6440,7 @@ class flameTimewarpML(flameMenuApp):
                             mask = (mask + (-m_)) / 2
                             del m_
 
-                        flow = 0.8 * flow + 0.2 * raft_flow
+                        flow = 0.75 * flow + 0.25 * raft_flow
 
                     else:
                         wf0 = warp(f0, flow[:, :2])
@@ -6461,9 +6461,9 @@ class flameTimewarpML(flameMenuApp):
                         flow = flow + fd
                         
                         if i == 2:
-                            flow = 0.9 * flow + 0.1 * raft_flow
+                            flow = 0.8 * flow + 0.2 * raft_flow
                         elif i == 3:
-                            flow = 0.95 * flow + 0.05 * raft_flow
+                            flow = 0.9 * flow + 0.1 * raft_flow
 
                         del fd
                         del m0
