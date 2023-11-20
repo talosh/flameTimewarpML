b1e155187b41d0c4e10ad5b973ddcd2f2d6a5de1
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-20 09:51:46 +0000

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -3701,7 +3701,7 @@ class flameTimewarpML(flameMenuApp):
         elif len (clip.versions[0].tracks[0].segments) != 1:
             return None
 
-        pprint (f'check_timewarp_effect: {effect}')
+        pprint (f'check_timewarp_effect: {effects}')
 
         for effect in effects:
             if effect.type == 'Timewarp':
