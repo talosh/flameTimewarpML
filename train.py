8e6d0e5191861d9ed244fe89ffbd0e3172e7f162
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-28 18:01:38 +0000

    update train.py

--- train.py
+++ train.py
@@ -222,6 +222,7 @@ class TimewarpMLDataset(torch.utils.data.Dataset):
             self.folders_with_exr
             file_name='dataset_folder.json'
             )
+        print (f'found {folders_with_descriptions} folders, {folders_to_scan} folders to scan')
 
         sys.exit()
 
