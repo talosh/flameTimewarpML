a50f6420843e5ca21d4282bf7fd0b9b3ffe537e9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-28 18:40:36 +0000

    update train.py

--- train.py
+++ train.py
@@ -346,14 +346,14 @@ class TimewarpMLDataset(torch.utils.data.Dataset):
 
         return folders_with_file, folders_without_file
 
-    def create_dataset_description(self, folder_path):
+    def create_dataset_description(self, folder_path, max_window=5):
         exr_files = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.exr')]
         exr_files.sort()
         try:
             first_exr_file_header = self.fw.read_openexr_file(exr_files[0], header_only = True)
             h = first_exr_file_header['shape'][0]
             w = first_exr_file_header['shape'][1]
-            print (f'\nshape: {h}:{w}')
+
         except Exception as e:
             print (f'\nError scanning {folder_path}: {e}')
         # pprint (first_exr_file_header)
