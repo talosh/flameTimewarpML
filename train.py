d78dad77229966942ba4759345cbd0f043402705
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-29 22:26:56 +0000

    update train.py

--- train.py
+++ train.py
@@ -349,7 +349,7 @@ class TimewarpMLDataset(torch.utils.data.Dataset):
 
         return folders_with_file, folders_without_file
 
-    def create_dataset_descriptions(self, folder_path, max_window=9):
+    def create_dataset_descriptions(self, folder_path, max_window=3):
 
         def sliding_window(lst, n):
             for i in range(len(lst) - n + 1):
