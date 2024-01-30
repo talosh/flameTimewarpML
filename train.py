32839f95eb8593bf84c9b5f33c19054ad695c19a
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-30 01:18:04 +0000

    update train.py

--- train.py
+++ train.py
@@ -864,6 +864,9 @@ def main():
 
             x = torch.cat((img1, img3, img2), dim=1)
             flow, mask, merged, teacher_res, loss_cons = model(x * 2 - 1, timestep = ratio)
+
+            print (type(merged))
+
             output = ( merged + 1 ) / 2
 
             loss = criterion_mse(output, img2)
