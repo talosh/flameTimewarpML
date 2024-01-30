e4e7fd1d434243e0d0ded5605f2c09035f9e5399
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-30 19:46:43 +0000

    update train.py

---
+++ retime_tree.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import random
+import struct
+import ctypes
+import argparse
+import importlib
+import queue
+import threading
+import time
+import platform
+
+from pprint import pprint
+
+import flameSimpleML_framework
+importlib.reload(flameSimpleML_framework)
+from flameSimpleML_framework import flameAppFramework
+
+settings = {
+    'menu_group_name': 'Simple ML',
+    'debug': False,
+    'app_name': 'flameSimpleML',
+    'prefs_folder': os.getenv('FLAMESMPLMsL_PREFS'),
+    'bundle_folder': os.getenv('FLAMESMPLML_BUNDLE'),
+    'packages_folder': os.getenv('FLAMESMPLML_PACKAGES'),
+    'temp_folder': os.getenv('FLAMESMPLML_TEMP'),
+    'requirements': [
+        'numpy>=1.16',
+        'torch>=1.12.0'
+    ],
+    'version': 'v0.0.3',
+}
+
+fw = flameAppFramework(settings = settings)
+importlib.invalidate_caches()
+try:
+    import numpy as np
+    import torch
+except:
+    if fw.site_packages_folder in sys.path:
+
+        print ('unable to import numpy and pytorch')
+        sys.exit()
+    else:
+        sys.path.append(fw.site_packages_folder)
+        try:
+            import numpy as np
+            import torch
+        except:
+            print ('unable to import numpy and pytorch')
+            sys.exit()
+        if fw.site_packages_folder in sys.path:
+            sys.path.remove(fw.site_packages_folder)
+
+import math
+
+import torch
+import torch.nn as nn
+from torch.optim.optimizer import Optimizer
+
+from models.flownet import FlownetCas
+from models.multires_v001 import Model as Model_01
+
+def main():
+    parser = argparse.ArgumentParser(description='Retime script.')
+
+    # Required argument
+    parser.add_argument('src_path', type=str, help='Path to the source tree')
+    parser.add_argument('dst_path', type=str, help='Path to the destination folder')
+
+    # Optional arguments
+    parser.add_argument('--model_path', type=str, default=None, help='Path to the pre-trained model (optional)')
+
+    # parser.add_argument('--rescan', action='store_true', help='Rescan the dataset (default: False)')
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
