2b231ae3f7094019dfa139dbe500746e23edb9b3
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-30 01:46:43 +0000

    update train.py

--- train.py
+++ train.py
@@ -933,7 +933,9 @@ def main():
             hours = int((epoch_time % (24 * 3600)) // 3600)
             minutes = int((epoch_time % 3600) // 60)
 
-            print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx + 1} / {len(dataset)}], Lr: {current_lr_str}, Loss L1: {loss_l1_str}', end='')
+            smoothed_loss = np.mean(moving_average(epoch_loss, 9))
+
+            print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx + 1} / {len(dataset)}], Lr: {current_lr_str}, Loss L1: {loss_l1_str}, Avg: {smoothed_loss:.6f}', end='')
             step = step + 1
 
         torch.save({
