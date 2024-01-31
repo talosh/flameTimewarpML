2d84f05966c2b35f45626857b0e4c51c9b433964
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-31 20:22:54 +0000

    update train.py

---
+++ convert_vimeo.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import random
+import struct
+import ctypes
+import argparse
+import importlib
+import queue
+import threading
+import time
+import platform
+
+import torch
+import torch.nn as nn
+
+import cv2
+
+from pprint import pprint
+
+def find_folders_with_png(path):
+    """
+    Find all folders under the given path that contain .exr files.
+
+    Parameters:
+    path (str): The root directory to start the search from.
+
+    Returns:
+    list: A list of directories containing .exr files.
+    """
+    directories_with_exr = set()
+
+    # Walk through all directories and files in the given path
+    for root, dirs, files in os.walk(path):
+        if root.endswith('preview'):
+            continue
+        for file in files:
+            if file.endswith('.png'):
+                directories_with_exr.add(root)
+                break  # No need to check other files in the same directory
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert vimeo 90k pngs to uncompreesed exrs.')
+    # Required argument
+    parser.add_argument('src_path', type=str, help='Path to the source tree')
+    parser.add_argument('dst_path', type=str, help='Path to the destination folder')
+    args = parser.parse_args()
+
+    folders_with_exr = find_folders_with_png(args.src_path)
+
+    pprint (folders_with_exr)
+
+
+if __name__ == "__main__":
+    main()
