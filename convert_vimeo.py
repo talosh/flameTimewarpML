088c80dcb8dfb97793ad945c85f8f45fb5fbd658
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-31 20:36:48 +0000

    update convert_vimeo.py and retime_tree.py

--- convert_vimeo.py
+++ convert_vimeo.py
@@ -40,6 +40,92 @@ def find_folders_with_png(path):
 
     return directories_with_png    
 
+def write_exr(image_data, filename, half_float = False, pixelAspectRatio = 1.0):
+    height, width, depth = image_data.shape
+    red = image_data[:, :, 0]
+    green = image_data[:, :, 1]
+    blue = image_data[:, :, 2]
+    if depth > 3:
+        alpha = image_data[:, :, 3]
+    else:
+        alpha = np.array([])
+
+    channels_list = ['B', 'G', 'R'] if not alpha.size else ['A', 'B', 'G', 'R']
+
+    MAGIC = 20000630
+    VERSION = 2
+    UINT = 0
+    HALF = 1
+    FLOAT = 2
+
+    def write_attr(f, name, type, value):
+        f.write(name.encode('utf-8') + b'\x00')
+        f.write(type.encode('utf-8') + b'\x00')
+        f.write(struct.pack('<I', len(value)))
+        f.write(value)
+
+    def get_channels_attr(channels_list):
+        channel_list = b''
+        for channel_name in channels_list:
+            name_padded = channel_name[:254] + '\x00'
+            bit_depth = 1 if half_float else 2
+            pLinear = 0
+            reserved = (0, 0, 0)  # replace with your values if needed
+            xSampling = 1  # replace with your value
+            ySampling = 1  # replace with your value
+            channel_list += struct.pack(
+                f"<{len(name_padded)}s i B 3B 2i",
+                name_padded.encode(), 
+                bit_depth, 
+                pLinear, 
+                *reserved, 
+                xSampling, 
+                ySampling
+                )
+        channel_list += struct.pack('c', b'\x00')
+
+            # channel_list += (f'{i}\x00').encode('utf-8')
+            # channel_list += struct.pack("<i4B", HALF, 1, 1, 0, 0)
+        return channel_list
+    
+    def get_box2i_attr(x_min, y_min, x_max, y_max):
+        return struct.pack('<iiii', x_min, y_min, x_max, y_max)
+
+    with open(filename, 'wb') as f:
+        # Magic number and version field
+        f.write(struct.pack('I', 20000630))  # Magic number
+        f.write(struct.pack('H', 2))  # Version field
+        f.write(struct.pack('H', 0))  # Version field
+        write_attr(f, 'channels', 'chlist', get_channels_attr(channels_list))
+        write_attr(f, 'compression', 'compression', b'\x00')  # no compression
+        write_attr(f, 'dataWindow', 'box2i', get_box2i_attr(0, 0, width - 1, height - 1))
+        write_attr(f, 'displayWindow', 'box2i', get_box2i_attr(0, 0, width - 1, height - 1))
+        write_attr(f, 'lineOrder', 'lineOrder', b'\x00')  # increasing Y
+        write_attr(f, 'pixelAspectRatio', 'float', struct.pack('<f', pixelAspectRatio))
+        write_attr(f, 'screenWindowCenter', 'v2f', struct.pack('<ff', 0.0, 0.0))
+        write_attr(f, 'screenWindowWidth', 'float', struct.pack('<f', 1.0))
+        f.write(b'\x00')  # end of header
+
+        # Scan line offset table size and position
+        line_offset_pos = f.tell()
+        pixel_data_start = line_offset_pos + 8 * height
+        bytes_per_channel = 2 if half_float else 4
+        # each scan line starts with 4 bytes for y coord and 4 bytes for pixel data size
+        bytes_per_scan_line = width * len(channels_list) * bytes_per_channel + 8 
+
+        for y in range(height):
+            f.write(struct.pack('<Q', pixel_data_start + y * bytes_per_scan_line))
+
+        channel_data = {'R': red, 'G': green, 'B': blue, 'A': alpha}
+
+        # Pixel data
+        for y in range(height):
+            f.write(struct.pack('I', y))  # Line number
+            f.write(struct.pack('I', bytes_per_channel * len(channels_list) * width))  # Pixel data size
+            for channel in sorted(channels_list):
+                f.write(channel_data[channel][y].tobytes())
+        f.close
+
 def main():
     parser = argparse.ArgumentParser(description='Convert vimeo 90k pngs to uncompreesed exrs.')
     # Required argument
@@ -53,15 +139,19 @@ def main():
         os.makedirs(args.dst_path)
 
     for folder_index, folder_path in enumerate(sorted(folders_with_png)):
-        # print (f'\rScanning folder {folder_index + 1} of {len(folders_with_png)}', end='')
+        print (f'\rScanning folder {folder_index + 1} of {len(folders_with_png)}', end='')
         png_files = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.png')]
         png_files.sort()
         for png_file_path in png_files:
             image_bgr = cv2.imread(png_file_path, cv2.IMREAD_COLOR)
             image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-            print (f'\nimage_rgb shape: {image_rgb.shape}')
+            exr_path = os.path.join(
+                args.dst_path,
+                os.path.dirname(png_file_path),
+                f'{os.path.splitext(os.path.basename(png_file_path))}.exr'
+            )
+            print (exr_path)
 
-    print ('')
 
 
 if __name__ == "__main__":
