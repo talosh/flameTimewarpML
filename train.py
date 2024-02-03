31c4a69c0b19538edeac1df545e0e8c18a01442c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-02-03 17:13:05 +0000

    update train.py

--- train.py
+++ train.py
@@ -1152,11 +1152,11 @@ def main():
             # loss = criterion_mse(output_yuv_gamma, target_yuv_gamma) # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
             loss_mse = criterion_mse(output, target) # * 0.6 + criterion_mse(output_blurred, target_blurred) * 0.4 # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
             # loss_l1 = criterion_l1(output, target)
-            loss_l1_measeure = criterion_l1(output.detach().copy(), target.detach().copy())
-            loss_l1_str = str(f'{loss_l1_measeure.item():.6f}')
+            loss_l1_disp = criterion_l1(output.detach().copy(), target.detach().copy())
+            loss_l1_str = str(f'{loss_l1_disp.item():.6f}')
 
-            epoch_loss.append(float(loss_l1_measeure))
-            steps_loss.append(float(loss_l1_measeure))
+            epoch_loss.append(float(loss_l1_disp))
+            steps_loss.append(float(loss_l1_disp))
 
             loss_mse.backward()
 
