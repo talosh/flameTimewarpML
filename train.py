94dabb6b5e86d082210cda230f11d287209f0ebd
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-02-03 20:18:37 +0000

    update train.py

--- train.py
+++ train.py
@@ -844,7 +844,7 @@ def split_to_yuv(rgb_tensor):
 
     return y_tensor, u_tensor, v_tensor
 
-def gamma_up(img, gamma = 1.28):
+def gamma_up(img, gamma = 1.18):
     return torch.sign(img) * torch.pow(torch.abs(img), 1 / gamma )
 
 def blur(img, interations = 16):
