8c147fe04174c4ad4eb910bcb51fb622ac2629b1
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-02-03 16:01:53 +0000

    update train.py

--- train.py
+++ train.py
@@ -1142,15 +1142,13 @@ def main():
             target = output_rife
 
             output_gamma = gamma_up(restore_normalized_values(output))
-            output_blurred = blur(restore_normalized_values(output))
-            output_yuv_gamma = torch.cat(split_to_yuv(output_gamma), dim=1)
+            output_yuv_gamma = normalize(torch.cat(split_to_yuv(output_gamma), dim=1))
 
             target_gamma = gamma_up(target)
-            target_blurred = blur(restore_normalized_values(target))
-            target_yuv_gamma = torch.cat(split_to_yuv(target_gamma), dim=1)
+            target_yuv_gamma = normalize(torch.cat(split_to_yuv(target_gamma), dim=1))
 
             # loss = criterion_mse(output_yuv_gamma, target_yuv_gamma) # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
-            loss = criterion_mse(output, target) # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
+            loss = criterion_mse(output_yuv_gamma, target_yuv_gamma) # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
             loss_l1 = criterion_l1(output, output_rife)
             loss_l1_str = str(f'{loss_l1.item():.6f}')
 
