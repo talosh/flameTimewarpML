22d72130215bcb31eb606091bca481fe3f690fa3
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-02-03 17:17:47 +0000

    update train.py

--- train.py
+++ train.py
@@ -1150,8 +1150,8 @@ def main():
             target_blurred = blur(output)
 
             # loss = criterion_mse(output_yuv_gamma, target_yuv_gamma) # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
-            loss_mse = criterion_mse(output, target) # * 0.6 + criterion_mse(output_blurred, target_blurred) * 0.4 # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
-            # loss_l1 = criterion_l1(output, target)
+            # loss_mse = criterion_mse(output, target) # * 0.6 + criterion_mse(output_blurred, target_blurred) * 0.4 # * 0.8 + (criterion_mse(output_u, target_u) + criterion_mse(output_v, target_v)) * 0.2
+            loss_mse = criterion_l1(output_yuv_gamma, target_yuv_gamma)
             loss_l1_disp = criterion_l1(output.detach(), target.detach())
             loss_l1_str = str(f'{loss_l1_disp.item():.6f}')
 
