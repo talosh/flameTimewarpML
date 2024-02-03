fd8704fd947931ee952842585fb13a306a8c3b53
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-02-03 14:03:14 +0000

    update train.py

--- train.py
+++ train.py
@@ -845,7 +845,7 @@ def split_to_yuv(rgb_tensor):
     return y_tensor, u_tensor, v_tensor
 
 def conver_to_log(img):
-    return torch.sign(img) * torch.pow(torch.abs(img), 1 / 4 )
+    return torch.sign(img) * torch.pow(torch.abs(img), 1 / 2 )
 
 def blur(img, interations = 16):
     def gaussian_kernel(size, sigma):
