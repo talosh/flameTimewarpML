69a8df47fce21664d047e374012c1c8026481336
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-02-04 10:09:59 +0000

    update fusion_v001.py and refine_v001.py

--- models/refine_v001.py
+++ models/refine_v001.py
@@ -252,7 +252,7 @@ class Model:
 				return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
 
 			def forward(self, img0, img1, flow0, flow1, mask, timestep):
-				
+
 				w_img0 = self.warp(img0, flow0) * 2 - 1
 				w_img1 = self.warp(img1, flow0) * 2 - 1
 
@@ -278,6 +278,7 @@ class Model:
 				x_multires2 = self.multiresblock2(x_pool1)
 				x_multires2_img0 = self.multiresblock2_img(x_pool1_img0)
 				x_multires2_img1 = self.multiresblock2_img(x_pool1_img1)
+				print (f'flow0 {flow0.shape}, enc: {x_multires2_img0.shape}')
 				x_multires2 = torch.cat((
 						self.warp(x_multires2_img0, flow0),
 						x_multires2,
@@ -295,6 +296,8 @@ class Model:
 				x_multires3 = self.multiresblock3(x_pool2)
 				x_multires3_img0 = self.multiresblock3_img(x_pool2_img0)
 				x_multires3_img1 = self.multiresblock3_img(x_pool2_img1)
+				print (f'flow0 {flow0.shape}, enc: {x_multires3_img0.shape}')
+
 				x_multires3 = torch.cat((
 						self.warp(x_multires3_img0, flow0),
 						x_multires3,
@@ -312,6 +315,8 @@ class Model:
 				x_multires4 = self.multiresblock4(x_pool3)
 				x_multires4_img0 = self.multiresblock4_img(x_pool3_img0)
 				x_multires4_img1 = self.multiresblock4_img(x_pool3_img1)
+				print (f'flow0 {flow0.shape}, enc: {x_multires4_img0.shape}')
+
 				x_multires4 = torch.cat((
 						self.warp(x_multires4_img0, flow0),
 						x_multires4,
