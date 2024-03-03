94eea2f7a5b11f620494b0c6ec332e1ea4ce7827
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-03-03 20:38:42 +0000

    update train.py

--- train.py
+++ train.py
@@ -1418,15 +1418,14 @@ def main():
         days = int(epoch_time // (24 * 3600))
         hours = int((epoch_time % (24 * 3600)) // 3600)
         minutes = int((epoch_time % 3600) // 60)
-        index = str(idx + 1)
 
         clear_lines(2)
-        print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx+1}, {index} / {len(dataset)}], Lr: {current_lr_str}, Loss CM: {loss_cm_str}, Loss L1: {loss_l1_str}')
+        print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx+1}, {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss CM: {loss_cm_str}, Loss L1: {loss_l1_str}')
         print(f'\r[Last 1K steps] Min: {window_min:.6f} Avg: {smoothed_window_loss:.6f}, Max: {window_max:.6f} [Epoch] Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f}')
 
         if epoch == 0:
             continue
-        if idx == 0:
+        if ( idx + 1 ) == len(dataset):
             torch.save({
                 'step': step,
                 'steps_loss': steps_loss,
