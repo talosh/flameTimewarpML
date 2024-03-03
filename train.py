9c133afe92955d463ae439d8c1d25bfe03fdcf9d
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-03-03 20:27:49 +0000

    update train.py

--- train.py
+++ train.py
@@ -1419,16 +1419,6 @@ def main():
         hours = int((epoch_time % (24 * 3600)) // 3600)
         minutes = int((epoch_time % 3600) // 60)
 
-        if idx == 0:
-            print ('hello')
-
-        # clear_lines(2)
-        # print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx+1}, {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss CM: {loss_cm_str}, Loss L1: {loss_l1_str}')
-        # print(f'\r[Last 1K steps] Min: {window_min:.6f} Avg: {smoothed_window_loss:.6f}, Max: {window_max:.6f} [Epoch] Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f}')
-
-        batch_idx = batch_idx + 1
-        step = step + 1
-
         if epoch == 0:
             continue
         if idx == 0:
@@ -1465,6 +1455,13 @@ def main():
             epoch_loss = []
             epoch = epoch + 1
             batch_idx = 0
+
+        clear_lines(2)
+        print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx+1}, {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss CM: {loss_cm_str}, Loss L1: {loss_l1_str}')
+        print(f'\r[Last 1K steps] Min: {window_min:.6f} Avg: {smoothed_window_loss:.6f}, Max: {window_max:.6f} [Epoch] Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f}')
+
+        batch_idx = batch_idx + 1
+        step = step + 1
 
 if __name__ == "__main__":
     main()
