8c06faae357fd6a9274069f708383c2d88271b2e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-03-06 02:17:32 +0000

    update train.py

--- train.py
+++ train.py
@@ -866,7 +866,8 @@ def warp_tenflow(tenInput, tenFlow):
         tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
         tenVertical = torch.linspace(-1.0, 1.0, tenFlow.shape[2]).view(1, 1, tenFlow.shape[2], 1).expand(tenFlow.shape[0], -1, -1, tenFlow.shape[3])
         backwarp_tenGrid[k] = torch.cat([ tenHorizontal, tenVertical ], 1).to(device = tenInput.device, dtype = tenInput.dtype)
-    g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
+        id_flow = torch.cat([ tenHorizontal, tenVertical ], 1).to(device = tenInput.device, dtype = tenInput.dtype)
+    g = (backwarp_tenGrid[k] + (tenFlow - id_flow)).permute(0, 2, 3, 1)
     return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
 
 def id_flow(tenInput):
