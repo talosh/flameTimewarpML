a3ac2cf89979f7e6c08ad92d0581348bf6b047da
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-03-27 23:05:00 +0000

    update flownet_v001.py

--- models/flownet_v001.py
+++ models/flownet_v001.py
@@ -26,7 +26,7 @@ class Model:
 			if k not in backwarp_tenGrid:
 				tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
 				tenVertical = torch.linspace(-1.0, 1.0, tenFlow.shape[2]).view(1, 1, tenFlow.shape[2], 1).expand(tenFlow.shape[0], -1, -1, tenFlow.shape[3])
-				backwarp_tenGrid[k] = torch.cat([ tenHorizontal, tenVertical ], 1).to(device)
+				backwarp_tenGrid[k] = torch.cat([ tenHorizontal, tenVertical ], 1).to(device=tenInput.device)
 				# end
 
 			tenFlow = torch.cat([ tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0), tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0) ], 1)
