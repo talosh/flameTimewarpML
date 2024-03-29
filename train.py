9073bf2f0b50f0558140b4540f1d63403dc79cb3
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-03-29 01:55:38 +0000

    update train.py

--- train.py
+++ train.py
@@ -1315,10 +1315,10 @@ def main():
             # rife_psnr = np.array(rife_psnr_list).mean()
             psnr = np.array(psnr_list).mean()
 
-            clear_lines(2)
-            # print (f'\r {" "*120}', end='')
+            clear_lines(3)
+            # print (f'\r {" "*240}', end='')
             print(f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f}, [PNSR] {psnr:.4f}')
-            print ('\n')
+            print ('\n\n')
             steps_loss = []
             epoch_loss = []
             epoch = epoch + 1
