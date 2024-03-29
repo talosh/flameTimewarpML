2e6b3aaba53323fc3deda45e9b39fd5de3982c20
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-03-29 10:28:46 +0000

    update train.py

--- train.py
+++ train.py
@@ -1197,7 +1197,7 @@ def main():
         hours = int((epoch_time % (24 * 3600)) // 3600)
         minutes = int((epoch_time % 3600) // 60)
 
-        clear_lines(3)
+        clear_lines(4)
         print (f'\r {" "*180}', end='')
         print ('\n')
         print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx+1}, {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss L1: {loss_l1_str}')
