de41824794a19dbc1e460e534b13b62f4d307d32
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-01 16:59:04 +0100

    update flownet_v004.py

--- models/flownet_v004.py
+++ models/flownet_v004.py
@@ -42,12 +42,12 @@ class Model:
 		class Head(Module):
 			def __init__(self):
 				super().__init__()
-				c = 44
+				c = 36
 				self.cnn0 = torch.nn.Conv2d(3, c, 3, 2, 1)
 				self.cnn1 = torch.nn.Conv2d(c, c, 5, 1, 2)
 				self.cnn2 = torch.nn.Conv2d(c, c, 5, 1, 2)
 				self.cnn3 = torch.nn.Conv2d(c, c, 5, 1, 2)
-				self.cnn4 = torch.nn.ConvTranspose2d(c, 11, 4, 2, 1)
+				self.cnn4 = torch.nn.ConvTranspose2d(c, 9, 4, 2, 1)
 				# self.relu = torch.nn.LeakyReLU(0.2, True)
 				self.relu = torch.nn.SELU(inplace = True)
 				# self.relu = torch.nn.Tanh()
@@ -129,10 +129,10 @@ class Model:
 		class FlownetCas(Module):
 			def __init__(self):
 				super().__init__()
-				self.block0 = Flownet(7+22, c=256)
-				self.block1 = Flownet(8+4+22, c=192)
-				self.block2 = Flownet(8+4+22, c=128)
-				self.block3 = Flownet(8+4+22, c=96)
+				self.block0 = Flownet(7+18, c=256)
+				self.block1 = Flownet(8+4+18, c=192)
+				self.block2 = Flownet(8+4+18, c=128)
+				self.block3 = Flownet(8+4+18, c=96)
 				self.encode = Head()
 
 			def forward(self, img0, gt, img1, f0, f1, timestep=0.5, scale=[8, 4, 2, 1]):
