ff1c090a1398b73f0d6ee5f941c4c94cd39ad7e4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-06 10:54:49 +0100

    update train.py

--- train.py
+++ train.py
@@ -1184,7 +1184,7 @@ def main():
             [1, 1, 1, 1],
         ]
 
-        if random.uniform(0, 1) < 0.4:
+        if random.uniform(0, 1) < 0.44:
             training_scale = random_scales[random.randint(0, len(random_scales) - 1)]
         else:
             training_scale = [8, 4, 2, 1]
