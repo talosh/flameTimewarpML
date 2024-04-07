4830f235a10d34665105c1f773e40d7117d1498f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-07 18:49:29 +0100

    delete 244 files and update 1 file

--- ruby_tw_parser/flame_channel_parser/lib/framecurve.rb
+++
@@ -1,9 +0,0 @@
-module Framecurve
-  # Is raised when a malformed framecurve bit has occurred in the system
-  class Malformed < RuntimeError
-  end
-end
-
-%w( version tuple comment curve parser validator serializer extractors/fcp_xml ).each do | f |
-  require File.join(File.dirname(__FILE__), "framecurve", f)
-end
