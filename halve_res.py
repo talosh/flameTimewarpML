4e40fa579096584b8e5ef6b0afe1eb276708319f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-07 13:14:03 +0100

    update halve_res.py

--- halve_res.py
+++ halve_res.py
@@ -2,6 +2,32 @@ import os
 import sys
 import argparse
 
+def find_folders_with_exr(path):
+    """
+    Find all folders under the given path that contain .exr files.
+
+    Parameters:
+    path (str): The root directory to start the search from.
+
+    Returns:
+    list: A list of directories containing .exr files.
+    """
+    directories_with_exr = set()
+
+    # Walk through all directories and files in the given path
+    for root, dirs, files in os.walk(path):
+        if root.endswith('preview'):
+            continue
+        if root.endswith('eval'):
+            continue
+        for file in files:
+            if file.endswith('.exr'):
+                directories_with_exr.add(root)
+                break  # No need to check other files in the same directory
+
+    return directories_with_exr
+
+
 def main():
     parser = argparse.ArgumentParser(description='Training script.')
 
@@ -9,7 +35,9 @@ def main():
     parser.add_argument('dataset_path', type=str, help='Path to the dataset')
     args = parser.parse_args()
 
-    print (args.dataset_path)
+    folders_with_exr = find_folders_with_exr(args.dataset_path)
+
+    print (folders_with_exr)
 
 if __name__ == "__main__":
     main()
