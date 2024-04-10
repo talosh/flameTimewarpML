7707b4e6c58f51cb7b5727ab1123433a9fab9b17
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-10 22:00:03 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -340,7 +340,7 @@ class ApplyModelDialog():
 
             import json
             try:
-                with open(file_path, 'w') as lockfile_path:
+                with open(lockfile_path, 'w') as json_file:
                     json.dump(data, json_file, indent=4)
             except Exception as e:
                 dialog = flame.messages.show_in_dialog(
