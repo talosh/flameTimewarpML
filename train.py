800da8d525750ce8eae47bd86bd78c54f18d5f65
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-11 06:53:10 +0100

    update train.py

--- train.py
+++ train.py
@@ -1397,7 +1397,7 @@ def main():
                 psnr_list = []
 
                 try:
-                    for ev_item_index in range(111):
+                    for ev_item_index in range(args.eval):
 
                         clear_lines(2)
                         print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [{batch_idx+1}, {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss L1: {loss_l1_str}')
