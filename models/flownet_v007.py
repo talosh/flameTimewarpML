bea1710821e6b7b39e30b63e2b923ec00a36af58
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-11 07:18:06 +0100

    update flownet_v007.py

--- models/flownet_v007.py
+++ models/flownet_v007.py
@@ -130,11 +130,13 @@ class Model:
 			def forward(self, img0, img1, f0, f1, timestep, mask, flow, scale=1, encode=None):
 				img0 = torch.nn.functional.interpolate(img0, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
 				img1 = torch.nn.functional.interpolate(img1, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
-				f0 = torch.nn.functional.interpolate(f0, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
-				f1 = torch.nn.functional.interpolate(f1, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
+				f0 = encode(f0)
+				f1 = encode(f1)
 				timestep = (img0[:, :1].clone() * 0 + 1) * timestep
 				x = torch.cat((img0, img1, f0, f1, timestep), 1)
 				if flow is not None:
+					f0 = torch.nn.functional.interpolate(f0, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
+					f1 = torch.nn.functional.interpolate(f1, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
 					mask = torch.nn.functional.interpolate(mask, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
 					flow = torch.nn.functional.interpolate(flow, scale_factor= 1. / scale, mode="bilinear", align_corners=False) * 1. / scale
 					x = torch.cat((img0, img1, f0, f1, timestep, mask, flow), 1)
