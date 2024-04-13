1e835300f8c9b908bb386aa68d2a6528fb84db19
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-13 22:54:42 +0100

    update flameTimewarpML_validate.py and flownet2_v004.py

--- pytorch/models/flownet2_v004.py
+++ pytorch/models/flownet2_v004.py
@@ -262,7 +262,7 @@ class Model:
                 c1[1] = c1[1].detach().to(device=torch.device('mps'))
                 c1[2] = c1[2].detach().to(device=torch.device('mps'))
                 c1[3] = c1[3].detach().to(device=torch.device('mps'))
-                # self.fusionnet.to(device=torch.device('mps'))
+                self.fusionnet.to(device=torch.device('mps'))
 
                 flow = torch.nn.functional.interpolate(flow, scale_factor=2.0, mode="bilinear",
                                     align_corners=False) * 2.0
