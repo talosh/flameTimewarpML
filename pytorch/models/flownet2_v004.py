f14b518a890bec10a042fac507a2ed3acf985068
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-13 22:24:40 +0100

    update flownet2_v004.py

--- pytorch/models/flownet2_v004.py
+++ pytorch/models/flownet2_v004.py
@@ -37,8 +37,8 @@ class Model:
             tenFlow = torch.cat([ tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0), tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0) ], 1)
 
             g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
-            result = torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='zeros', align_corners=True)
-            return result.detach().to(device=torch.device('mps'))
+            result = torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
+            return result.detach().to(device=input_device)
         # '''
 
         '''
