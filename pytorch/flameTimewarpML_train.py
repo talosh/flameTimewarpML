4fce43f93825cadb746671da5f3ce5385b778965
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-15 22:32:25 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1075,6 +1075,157 @@ def find_and_import_model(models_dir='models', base_name=None, model_name=None):
         print(f"Model not found: {base_name or model_name}")
         return None
 
+### U-Net Discriminator ###
+# Residual block for the discriminator
+class DBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, which_conv=nn.Conv2d, which_bn=nn.BatchNorm2d, wide=True,
+                preactivation=True, activation=nn.LeakyReLU(0.1, inplace=False), downsample=nn.AvgPool2d(2, stride=2)):
+        super(DBlock, self).__init__()
+        self.in_channels, self.out_channels = in_channels, out_channels
+        # If using wide D (as in SA-GAN and BigGAN), change the channel pattern
+        self.hidden_channels = self.out_channels if wide else self.in_channels
+        self.which_conv, self.which_bn = which_conv, which_bn
+        self.preactivation = preactivation
+        self.activation = activation
+        self.downsample = downsample
+            
+        # Conv layers
+        self.conv1 = self.which_conv(self.in_channels, self.hidden_channels, kernel_size=3, padding=1)
+        self.conv2 = self.which_conv(self.hidden_channels, self.out_channels, kernel_size=3, padding=1)
+        self.learnable_sc = True if (in_channels != out_channels) or downsample else False
+        if self.learnable_sc:
+            self.conv_sc = self.which_conv(in_channels, out_channels, 
+                                            kernel_size=1, padding=0)
+
+        self.bn1 = self.which_bn(self.hidden_channels)
+        self.bn2 = self.which_bn(out_channels)
+
+    # def shortcut(self, x):
+    #     if self.preactivation:
+    #         if self.learnable_sc:
+    #             x = self.conv_sc(x)
+    #         if self.downsample:
+    #             x = self.downsample(x)
+    #     else:
+    #         if self.downsample:
+    #             x = self.downsample(x)
+    #         if self.learnable_sc:
+    #             x = self.conv_sc(x)
+    #     return x
+        
+    def forward(self, x):
+        if self.preactivation:
+            # h = self.activation(x) # NOT TODAY SATAN
+            # Andy's note: This line *must* be an out-of-place ReLU or it 
+            #              will negatively affect the shortcut connection.
+            h = self.activation(x)
+        else:
+            h = x    
+        h = self.bn1(self.conv1(h))
+        # h = self.conv2(self.activation(h))
+        if self.downsample:
+            h = self.downsample(h)     
+            
+        return h #+ self.shortcut(x)
+        
+
+class GBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                which_conv=nn.Conv2d, which_bn=nn.BatchNorm2d, activation=nn.LeakyReLU(0.1, inplace=False), 
+                upsample=nn.Upsample(scale_factor=2, mode='nearest')):
+        super(GBlock, self).__init__()
+        
+        self.in_channels, self.out_channels = in_channels, out_channels
+        self.which_conv, self.which_bn = which_conv, which_bn
+        self.activation = activation
+        self.upsample = upsample
+        # Conv layers
+        self.conv1 = self.which_conv(self.in_channels, self.out_channels, kernel_size=3, padding=1)
+        self.conv2 = self.which_conv(self.out_channels, self.out_channels, kernel_size=3, padding=1)
+        self.learnable_sc = in_channels != out_channels or upsample
+        if self.learnable_sc:
+            self.conv_sc = self.which_conv(in_channels, out_channels, 
+                                            kernel_size=1, padding=0)
+        # Batchnorm layers
+        self.bn1 = self.which_bn(out_channels)
+        self.bn2 = self.which_bn(out_channels)
+        # upsample layers
+        self.upsample = upsample
+
+    def forward(self, x):
+        h = self.activation(x)
+        if self.upsample:
+            h = self.upsample(h)
+            # x = self.upsample(x)
+        h = self.bn1(self.conv1(h))
+        # h = self.activation(self.bn2(h))
+        # h = self.conv2(h)
+        # if self.learnable_sc:       
+        #     x = self.conv_sc(x)
+        return h #+ x
+
+
+class UnetD(torch.nn.Module):
+    def __init__(self):
+        super(UnetD, self).__init__()
+
+        self.enc_b1 = DBlock(3, 64, preactivation=False)
+        self.enc_b2 = DBlock(64, 128)
+        self.enc_b3 = DBlock(128, 192)
+        self.enc_b4 = DBlock(192, 256)
+        self.enc_b5 = DBlock(256, 320)
+        self.enc_b6 = DBlock(320, 384)
+
+        self.enc_out = nn.Conv2d(384, 1, kernel_size=1, padding=0)
+
+        self.dec_b1 = GBlock(384, 320)
+        self.dec_b2 = GBlock(320*2, 256)
+        self.dec_b3 = GBlock(256*2, 192)
+        self.dec_b4 = GBlock(192*2, 128)
+        self.dec_b5 = GBlock(128*2, 64)
+        self.dec_b6 = GBlock(64*2, 32)
+
+        self.dec_out = nn.Conv2d(32, 1, kernel_size=1, padding=0)
+
+        # Init weights
+        for m in self.modules():
+            classname = m.__class__.__name__
+            if classname.lower().find('conv') != -1:
+                # print(classname)
+                nn.init.kaiming_normal(m.weight)
+                nn.init.constant(m.bias, 0)
+            elif classname.find('bn') != -1:
+                m.weight.data.normal_(1.0, 0.02)
+                m.bias.data.fill_(0)
+
+    def forward(self, x):
+        e1 = self.enc_b1(x)
+        e2 = self.enc_b2(e1)
+        e3 = self.enc_b3(e2)
+        e4 = self.enc_b4(e3)
+        e5 = self.enc_b5(e4)
+        e6 = self.enc_b6(e5)
+
+        e_out = self.enc_out(F.leaky_relu(e6, 0.1))
+        # print(e1.size())
+        # print(e2.size())
+        # print(e3.size())
+        # print(e4.size())
+        # print(e5.size())
+        # print(e6.size())
+
+        d1 = self.dec_b1(e6)
+        d2 = self.dec_b2(torch.cat([d1, e5], 1))
+        d3 = self.dec_b3(torch.cat([d2, e4], 1))
+        d4 = self.dec_b4(torch.cat([d3, e3], 1))
+        d5 = self.dec_b5(torch.cat([d4, e2], 1))
+        d6 = self.dec_b6(torch.cat([d5, e1], 1))
+
+        d_out = self.dec_out(F.leaky_relu(d6, 0.1))
+
+        return e_out, d_out, [e1,e2,e3,e4,e5,e6], [d1,d2,d3,d4,d5,d6]
+
+
 # def init_weights(m):
 #     if isinstance(m, torch.nn.Linear):
 
@@ -1120,10 +1271,14 @@ def main():
     if not model_info.get('ratio_support'):
         max_dataset_window = 3
     flownet = Flownet().get_training_model()().to(device)
+    model_D = UnetD().to(device)
     if args.all_gpus:
         print ('Using nn.DataParallel')
         flownet = torch.nn.DataParallel(flownet)
         flownet.to(device)
+        model_D = torch.nn.DataParallel(model_D)
+        model_D.to(device)
+
 
     if not os.path.isdir(os.path.join(args.dataset_path, 'preview')):
         os.makedirs(os.path.join(args.dataset_path, 'preview'))
@@ -1301,6 +1456,15 @@ def main():
         print (f'unable to load FlownetTeacher state: {e}')
     flownet_teacher.eval()
     '''
+
+    # LPIPS Init
+    import lpips
+    os.environ['TORCH_HOME'] = os.path.join(
+        os.path.abspath(__file__),
+        'pytorch'
+    )
+    loss_fn_alex = lpips.LPIPS(net='alex')
+
 
     while True:
         data_time = time.time() - time_stamp
