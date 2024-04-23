cf1544a11c9016c6de8dfe62c9e3cd02c87c9f4c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-04-23 21:38:58 +0100

    update flownet4_v001.py and flownet4_v002.py

--- pytorch/models/flownet4_v002.py
+++ pytorch/models/flownet4_v002.py
@@ -145,7 +145,7 @@ class Model:
         class ResConv(Module):
             def __init__(self, c, dilation=1):
                 super().__init__()
-                self.conv = torch.nn.Conv2d(c, c, 3, 1, dilation, dilation = dilation, groups = 1, padding_mode = 'reflect', bias=False)
+                self.conv = torch.nn.Conv2d(c, c, 3, 1, dilation, dilation = dilation, groups = 1, padding_mode = 'reflect', bias=True)
                 self.beta = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)        
                 self.relu = torch.nn.LeakyReLU(0.2, True) # torch.nn.SELU(inplace = True)
                 
