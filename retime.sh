98013bc9eddc75dc15761dae54be273201f18a59
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-08 21:11:56 +0100

    update 1 file and delete 1 file

---
+++ retime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Resolves the directory where the script is located
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Change to the script directory
+cd "$SCRIPT_DIR"
+
+# Define the path to the Python executable and the Python script
+PYTHON_CMD="./packages/.miniconda/appenv/bin/python"
+PYTHON_SCRIPT="./pytorch/retime.py"
+
+# Run the Python script with all arguments passed to this shell script
+$PYTHON_CMD $PYTHON_SCRIPT "$@"
