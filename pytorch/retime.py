5127972fcb4de7488eb7d34ecbcd84a385006255
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-12 21:47:06 +0100

    update retime.py

--- pytorch/retime.py
+++ pytorch/retime.py
@@ -17,6 +17,285 @@ except:
         print (f'Using {python_executable_path} python interpreter')
         sys.exit()
 
+class MinExrReader:
+    '''Minimal, standalone OpenEXR reader for single-part, uncompressed scan line files.
+
+    This OpenEXR reader makes a couple of assumptions
+    - single-part files with arbitrary number of channels,
+    - no pixel data compression, and
+    - equal channel types (HALF, FLOAT, UINT).
+
+    These assumptions allow us to efficiently parse and read the `.exr` file. In particular
+    we gain constant offsets between scan lines which allows us to read the entire image
+    in (H,C,W) format without copying.
+
+    Use `MinimalEXR.select` to select a subset of channels in the given order. `MinimalEXR.select`
+    tries to be smart when copying is required and when views are ok.
+    
+    Based on the file format presented in
+    https://www.openexr.com/documentation/openexrfilelayout.pdf
+
+    Attributes
+    ----------
+    shape: tuple
+        Shape of image in (H,C,W) order
+    image: numpy.array
+        Uncompressed image data.
+    attrs: dict
+        OpenEXR header attributes.
+    '''
+
+    class BufferReader:
+        '''A lightweight io.BytesIO object with convenience functions.
+        
+        Params
+        ------
+        data : bytes-like
+            Bytes for which random access is required.
+        
+        '''
+
+        def __init__(self, data):
+            self.data = data
+            self.len = len(data)
+            self.off = 0
+
+        def read(self, n):
+            '''Read next `n` bytes.'''
+            v = self.data[self.off:self.off+n]
+            self.off += n
+            return v
+
+        def read_null_string(self):
+            import ctypes
+            '''Read a null-terminated string.'''
+            s = ctypes.create_string_buffer(self.data[self.off:]).value
+            if s != None:
+                s = s.decode('utf-8')
+                self.off += len(s) + 1
+            return s
+
+        def peek(self):
+            '''Peek next byte.'''
+            return self.data[self.off]
+
+        def advance(self, n):
+            '''Advance offset by `n` bytes.'''
+            self.off += n
+
+        def nleft(self):
+            '''Returns the number of bytes left to read.'''
+            return self.len - self.off - 1
+
+    def __init__(self, fp, header_only = False):
+        self.fp = fp
+        self.image = None
+        self.shape = None
+
+        self._read_header()
+        if not header_only:
+            self._read_image()
+
+    def select(self, channels, channels_last=True):
+        import numpy as np
+        '''Returns an image composed only of the given channels.
+        
+        Attempts to be smart about memory views vs. memory copies.
+
+        Params
+        ------
+        channels: list-like
+            Names of channels to be extracted. Appearance in list
+            also defines the order of the channels. 
+        channels_last: bool, optional
+            When true return image in (H,W,C) format.
+
+        Returns
+        -------
+        image: HxWxC or HxCxW array
+            Selected image data.
+        '''
+        H,C,W = self.shape
+        ids = [self.channel_map[c] for c in channels]                
+        if len(ids) == 0:
+            img = np.empty((H,0,W), dtype=self.image.dtype)
+        else:
+            diff = np.diff(ids)
+            sH = slice(0, H)
+            sW = slice(0, W)
+            if len(diff) == 0:
+                # single channel select, return view
+                sC = slice(ids[0],ids[0]+1)
+                img = self.image[sH,sC,sW]
+            elif len(set(diff)) == 1:
+                # mutliple channels, constant offset between, return view
+                # Careful here with negative steps, ie. diff[0] < 0:
+                start = ids[0]
+                step = diff[0]
+                end = ids[-1]+diff[0]
+                end = None if end < 0 else end                
+                sC = slice(start,end,step)
+                img = self.image[sH,sC,sW]
+            else:
+                # multiple channels not slicable -> copy mem
+                chdata = [self.image[sH,i:i+1,sW] for i in ids]
+                img = np.concatenate(chdata, 1)
+        
+        if channels_last:
+            img = img.transpose(0,2,1)
+        return img
+
+    def _read_header(self):
+        import numpy as np
+        import struct
+
+        self.fp.seek(0)        
+        buf = self.BufferReader(self.fp.read(10000))
+
+        # Magic and version and info bits
+        magic, version, b2, b3, b4 = struct.unpack('<iB3B', buf.read(8))
+        assert magic == 20000630, 'Not an OpenEXR file.'
+        assert b2 in (0, 4), 'Not a single-part scan line file.'
+        assert b3 == b4 == 0, 'Unused flags in version field are not zero.'
+
+        # Header attributes
+        self.attrs = self._read_header_attrs(buf)
+
+        # Parse channels and datawindow
+        self.compr = self._parse_compression(self.attrs)        
+        self.channel_names, self.channel_types = self._parse_channels(self.attrs)
+        self.channel_map = {cn:i for i,cn in enumerate(self.channel_names)}
+        H, W = self._parse_data_window(self.attrs)
+        self.shape = (H,len(self.channel_names),W)
+        self.first_offset = self._read_first_offset(buf)
+        
+        # Assert our assumptions
+        assert self.compr == 0x00, 'Compression not supported.'
+        assert len(set(self.channel_types)) <= 1, 'All channel types must be equal.'
+
+    def _read_image(self):
+        import numpy as np
+        # Here is a shortcut: We assume all channels of the same type and thus constant offsets between
+        # scanlines (SOFF). Note, each scanline has a header (y-coordinate (int4), data size DS (int4)) and data in scanlines
+        # is stored consecutively for channels (in order of appearance in header). Thus we can interpret the content
+        # as HxCxW image with strides: (SOFF,DS*W,DS)
+        H,C,W = self.shape
+
+        if np.prod(self.shape) == 0:
+            return np.empty(self.shape, dtype=np.float32)
+
+        dtype  = self.channel_types[0]
+        DS = np.dtype(dtype).itemsize
+        SOFF = 8+DS*W*C        
+        strides = (SOFF, DS*W, DS)
+        nbytes = SOFF*H
+
+        self.fp.seek(self.first_offset, 0)
+        image = np.frombuffer(self.fp.read(nbytes), dtype=dtype, count=-1, offset=8)
+        self.image = np.lib.stride_tricks.as_strided(image, (H,C,W), strides)
+
+    def _read_header_attrs(self, buf):
+        attrs = {}
+        while buf.nleft() > 0:
+            attr = self._read_header_attr(buf)
+            if attr is None:
+                break
+            attrs[attr[0]] = attr
+        return attrs
+
+    def _read_header_attr(self, buf):
+        import struct
+        if buf.peek() == 0x00:
+            buf.advance(1)
+            return None
+        aname = buf.read_null_string()
+        atype = buf.read_null_string()
+        asize = struct.unpack('<i', buf.read(4))[0]
+        data = buf.read(asize)
+        return (aname, atype, asize, data)
+
+    def _parse_channels(self, attrs):
+        import struct
+        import numpy as np
+
+        attr = attrs['channels']
+        assert attr[1] == 'chlist', 'Unexcepted type for channels attribute.'
+        buf = self.BufferReader(attr[-1])
+        channel_names, channel_types = [], []
+        PT_LOOKUP = [np.uint32, np.float16, np.float32]
+        while buf.nleft() > 0 and buf.peek() != 0x00:            
+            channel_names.append(buf.read_null_string())
+            pt = struct.unpack('<i', buf.read(4))[0]
+            channel_types.append(PT_LOOKUP[pt])
+            buf.advance(12) # skip remaining entries
+        if buf.nleft() > 0:
+            buf.advance(1) # account for zero byte
+        return channel_names, channel_types
+
+    def _parse_data_window(self, attrs):
+        import struct
+        attr = attrs['dataWindow']
+        assert attr[1] == 'box2i', 'Unexcepted type for dataWindow attribute.'
+        xmin, ymin, xmax, ymax = struct.unpack('<iiii', attr[-1])
+        return (ymax-ymin+1, xmax-xmin+1)
+
+    def _parse_compression(self, attrs):
+        return attrs['compression'][-1][0]
+
+    def _read_offsets(self, buf):
+        import struct
+        offsets = []
+        while buf.nleft() > 0 and buf.peek() != 0x00:
+            o = struct.unpack('<Q', buf.read(8))[0]
+            offsets.append(o)
+        if buf.nleft() > 0:
+            buf.advance(1) # account for zero byte
+        return offsets
+
+    def _read_first_offset(self, buf):
+        import struct
+        assert buf.nleft() > 0 and buf.peek() != 0x00, 'Failed to read offset.'
+        return struct.unpack('<Q', buf.read(8))[0]
+
+def read_openexr_file(file_path, header_only = False):
+    """
+    Reads data from an OpenEXR file specified by the file path.
+
+    This function opens an OpenEXR file and reads its contents, either the header information only or the full data, including image data. It utilizes the MinExrReader to process the file.
+
+    Parameters:
+    - file_path (str): Path to the OpenEXR file to be read.
+    - header_only (bool, optional): If True, only header information is read. Defaults to False.
+
+    Returns:
+    - dict: A dictionary containing the OpenEXR file's metadata and image data (if header_only is False). The dictionary includes the following keys:
+        - 'attrs': Attributes of the OpenEXR file.
+        - 'compr': Compression type used in the OpenEXR file.
+        - 'channel_names': Names of the channels in the OpenEXR file.
+        - 'channel_types': Data types of the channels in the OpenEXR file.
+        - 'shape': The shape of the image data, rearranged as (height, width, channels).
+        - 'image_data': Numpy array of the image data if header_only is False. The data is transposed to match the shape (height, width, channels).
+
+    Note:
+    - The function uses a context manager to ensure the file is properly closed after reading.
+    - It assumes the existence of a class method `MinExrReader` for reading the OpenEXR file.
+    """
+
+    import numpy as np
+    with open(file_path, 'rb') as sfp:
+        source_reader = MinExrReader(sfp, header_only)
+        result = {
+            'attrs': source_reader.attrs,
+            'compr': source_reader.compr,
+            'channel_names': source_reader.channel_names,
+            'channel_types': source_reader.channel_types,
+            'shape': (source_reader.shape[0], source_reader.shape[2], source_reader.shape[1]),
+        }
+        if not header_only:
+            result['image_data'] = source_reader.image.transpose(0, 2, 1)[:, :, ::-1].copy()
+        del source_reader
+    return result
+
 def find_folders_with_exr(path):
     """
     Find all folders under the given path that contain .exr files.
@@ -189,8 +468,8 @@ def read_frames(all_frame_descriptions, frames_queue):
     while True:
         for description in all_frame_descriptions:
             try:
-                description['incoming_data'] = fw.read_openexr_file(description['incoming'])['image_data']
-                description['outgoing_data'] = fw.read_openexr_file(description['outgoing'])['image_data']
+                description['incoming_data'] = read_openexr_file(description['incoming'])['image_data']
+                description['outgoing_data'] = read_openexr_file(description['outgoing'])['image_data']
                 frames_queue.put(description)
             except Exception as e:
                 print (e)                
@@ -272,7 +551,7 @@ def main():
     print ('loading model...')
     device = torch.device("mps") if platform.system() == 'Darwin' else torch.device(f'cuda:{args.device}')
     try:
-        checkpoint = torch.load(trained_model_path, map_location=device)
+        checkpoint = torch.load(args.model_path, map_location=device)
         print('loaded previously saved model checkpoint')
     except Exception as e:
         print (f'unable to load saved model checkpoint: {e}')
@@ -284,7 +563,7 @@ def main():
     model = Flownet().to(device)
 
     model.load_state_dict(checkpoint['flownet_state_dict'])
-    model.half()
+    # model.half()
     model.eval()
 
     for frame_idx in range(len(all_frame_descriptions)):
@@ -308,6 +587,13 @@ def main():
         
         img0 = torch.nn.functional.pad(img0, padding)
         img1 = torch.nn.functional.pad(img1, padding)
+
+        flow_list, mask_list, merged = model(
+            img0, 
+            img1, 
+            frame_data['ratio'], 
+            iterations = args.iterations
+            )
 
         flow_list, mask, merged, teacher_res, loss_cons = model(torch.cat((img0, img1), dim=1), timestep = frame_data['ratio'])    
 
