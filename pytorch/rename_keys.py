cb342d6a31f884731526fead1686d64aa11cbed2
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-12 19:45:14 +0100

    update flownet4_v004m.py and rename_keys.py

--- pytorch/rename_keys.py
+++ pytorch/rename_keys.py
@@ -12,6 +12,44 @@ except:
         print (f'Using {python_executable_path} python interpreter')
         sys.exit()
 
+key_mapping_01 = {
+    'block0.encode01.0.weight': 'block0.encode01.downconv01.conv1.weight',
+    'block0.encode01.0.bias':   'block0.encode01.downconv01.conv1.bias',
+    'block0.encode01.2.weight': 'block0.encode01.conv01.conv1.weight',
+    'block0.encode01.2.bias':   'block0.encode01.conv01.conv1.bias',
+    'block0.encode01.4.weight': 'block0.encode01.conv02.conv1.weight',
+    'block0.encode01.4.bias':   'block0.encode01.conv02.conv1.bias',
+    'block0.encode01.6.weight': 'block0.encode01.upsample01.conv1.weight',
+    'block0.encode01.6.bias':   'block0.encode01.upsample01.conv1.bias',
+
+    'block1.encode01.0.weight': 'block1.encode01.downconv01.conv1.weight',
+    'block1.encode01.0.bias':   'block1.encode01.downconv01.conv1.bias',
+    'block1.encode01.2.weight': 'block1.encode01.conv01.conv1.weight',
+    'block1.encode01.2.bias':   'block1.encode01.conv01.conv1.bias',
+    'block1.encode01.4.weight': 'block1.encode01.conv02.conv1.weight',
+    'block1.encode01.4.bias':   'block1.encode01.conv02.conv1.bias',
+    'block1.encode01.6.weight': 'block1.encode01.upsample01.conv1.weight',
+    'block1.encode01.6.bias':   'block1.encode01.upsample01.conv1.bias',
+
+    'block2.encode01.0.weight': 'block2.encode01.downconv01.conv1.weight',
+    'block2.encode01.0.bias':   'block2.encode01.downconv01.conv1.bias',
+    'block2.encode01.2.weight': 'block2.encode01.conv01.conv1.weight',
+    'block2.encode01.2.bias':   'block2.encode01.conv01.conv1.bias',
+    'block2.encode01.4.weight': 'block2.encode01.conv02.conv1.weight',
+    'block2.encode01.4.bias':   'block2.encode01.conv02.conv1.bias',
+    'block2.encode01.6.weight': 'block2.encode01.upsample01.conv1.weight',
+    'block2.encode01.6.bias':   'block2.encode01.upsample01.conv1.bias',
+
+    'block3.encode01.0.weight': 'block3.encode01.downconv01.conv1.weight',
+    'block3.encode01.0.bias':   'block3.encode01.downconv01.conv1.bias',
+    'block3.encode01.2.weight': 'block3.encode01.conv01.conv1.weight',
+    'block3.encode01.2.bias':   'block3.encode01.conv01.conv1.bias',
+    'block3.encode01.4.weight': 'block3.encode01.conv02.conv1.weight',
+    'block3.encode01.4.bias':   'block3.encode01.conv02.conv1.bias',
+    'block3.encode01.6.weight': 'block3.encode01.upsample01.conv1.weight',
+    'block3.encode01.6.bias':   'block3.encode01.upsample01.conv1.bias',
+}
+
 def main():
     parser = argparse.ArgumentParser(description='Move exrs to corresponding folders')
 
@@ -20,11 +58,21 @@ def main():
     parser.add_argument('dest', type=str, help='Path to dest state dict')
     args = parser.parse_args()
 
-    src_checkpoint = torch.load(args.source)
-    src_state_dict = src_checkpoint.get('flownet_state_dict')
+    checkpoint = torch.load(args.source)
+    src_state_dict = checkpoint.get('flownet_state_dict')
+    
+    dest_state_dict = {}    
+    for key in src_state_dict:
+        new_key = key_mapping_01.get(key, key)
+        dest_state_dict[new_key] = src_state_dict[key]
 
+    checkpoint['flownet_state_dict'] = dest_state_dict
+    torch.save(checkpoint, args.dest)
+
+    '''
     for key in src_state_dict.keys():
         print (key)
+    '''
 
 if __name__ == "__main__":
     main()
