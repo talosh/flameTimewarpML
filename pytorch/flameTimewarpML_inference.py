6408b3cd8a0b739bd3ea17b6770056878becd45e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-13 22:23:45 +0100

    update flameTimewarpML_inference.py

--- pytorch/flameTimewarpML_inference.py
+++ pytorch/flameTimewarpML_inference.py
@@ -491,8 +491,7 @@ class Timewarp():
             image_path = frame_info['output']
             write_image_queue.put({'image_data': image_data, 'image_path': image_path})
 
-        print ('finished - waiting for write thread')
-
+        write_image_queue.put({'image_data': None, 'image_path': None})
         write_thread.join()
         return True
    
