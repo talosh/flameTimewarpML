3f01fbccbdab2cd4a9a92d024d8aa553ae2c104c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-14 01:09:00 +0100

    update flameTimewarpML_inference.py

--- pytorch/flameTimewarpML_inference.py
+++ pytorch/flameTimewarpML_inference.py
@@ -537,11 +537,6 @@ class Timewarp():
             ratio = frame_info['ratio']
             image_path = frame_info['output']
             try:
-                if warmup:
-                    result = self.predict(img0, img1, ratio = ratio, iterations = 1)
-                    write_image_queue.put({'image_data': result.copy(), 'image_path': image_path})
-                    del result
-                    warmup = False
                 result = self.predict(img0, img1, ratio = ratio, iterations = 1)
                 write_image_queue.put({'image_data': result.copy(), 'image_path': image_path})
                 del result
