ea3601d86df9bddf2dc45a0e31c6bde093943a87
Your Name <you@example.com>
2024-05-15 17:47:06 +0100

    added photometric weight

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1656,26 +1656,27 @@ def main():
         # warped_img2 = warp(img2, flow_list[3][:, 2:4])
         # output = warped_img0 * mask_list[3] + warped_img2 * (1 - mask_list[3])
 
-        lpips_weight = 4e-4
+        pm_weight = 0.1
+        lpips_weight = 0.9
         x8_output = torch.nn.functional.interpolate(restore_normalized_values(merged[0]), scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_orig = torch.nn.functional.interpolate(img1_orig, scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_lpips = torch.mean(loss_fn_alex(x8_output * 2 - 1, x8_orig * 2 - 1))
-        loss_x8 = criterion_huber(x8_output, x8_orig) + lpips_weight * x8_lpips
+        loss_x8 = pm_weight * criterion_huber(x8_output, x8_orig) + lpips_weight * x8_lpips
 
         x4_output = torch.nn.functional.interpolate(restore_normalized_values(merged[1]), scale_factor= 1. / training_scale[1], mode="bilinear", align_corners=False)
         x4_orig = torch.nn.functional.interpolate(img1_orig, scale_factor= 1. / training_scale[1], mode="bilinear", align_corners=False)
         x4_lpips = torch.mean(loss_fn_alex(x4_output * 2 - 1, x4_orig * 2 - 1))
-        loss_x4 = criterion_huber(x4_output, x4_orig) + lpips_weight * x4_lpips
+        loss_x4 = pm_weight * criterion_huber(x4_output, x4_orig) + lpips_weight * x4_lpips
 
         x2_output = torch.nn.functional.interpolate(restore_normalized_values(merged[2]), scale_factor= 1. / training_scale[2], mode="bilinear", align_corners=False)
         x2_orig = torch.nn.functional.interpolate(img1_orig, scale_factor= 1. / training_scale[2], mode="bilinear", align_corners=False)
         x2_lpips = torch.mean(loss_fn_alex(x2_output * 2 - 1, x2_orig * 2 - 1))
-        loss_x2 = criterion_huber(x2_output, x2_orig) + lpips_weight * x2_lpips
+        loss_x2 = pm_weight * criterion_huber(x2_output, x2_orig) + lpips_weight * x2_lpips
 
         x1_output = restore_normalized_values(merged[3])
         x1_orig = img1_orig
         x1_lipis = torch.mean(loss_fn_alex(x1_output * 2 - 1, x1_orig * 2 - 1))
-        loss_x1 = criterion_huber(x1_output, x1_orig) + lpips_weight * x1_lipis
+        loss_x1 = pm_weight * criterion_huber(x1_output, x1_orig) + lpips_weight * x1_lipis
 
         loss = 0.28 * loss_x8 + 0.12 * loss_x4 + 0.12 * loss_x2 + 0.48 * loss_x1
 
