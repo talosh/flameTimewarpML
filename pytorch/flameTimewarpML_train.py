1401f9ac62857e48f73146bc18daea77a0ac0405
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-15 19:21:32 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1657,7 +1657,7 @@ def main():
         # output = warped_img0 * mask_list[3] + warped_img2 * (1 - mask_list[3])
 
         pm_weight = 0
-        lpips_weight = 0.1
+        lpips_weight = 0.9
         x8_output = torch.nn.functional.interpolate(restore_normalized_values(merged[0]), scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_orig = torch.nn.functional.interpolate(img1_orig, scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_lpips = torch.mean(loss_fn_alex(x8_output * 2 - 1, x8_orig * 2 - 1))
