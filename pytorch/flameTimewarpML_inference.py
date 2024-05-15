5b5c02744d8a28cc080b8637c3ddf45da02d842d
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-15 22:04:00 +0100

    update flameTimewarpML_inference.py

--- pytorch/flameTimewarpML_inference.py
+++ pytorch/flameTimewarpML_inference.py
@@ -757,7 +757,7 @@ class Timewarp():
                         [-2, 3,  0,  0],
                         [1, -2,  1,  0],
                         [1, -1,  0,  0]
-                    ])
+                    ]).T
 
                     '''
                     self.HERMATRIX = np.array([
