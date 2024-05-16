f07de50f2aa6ede977ab912ddd9f0512ec8de969
Your Name <you@example.com>
2024-05-16 10:36:22 +0100

    weight balance

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1685,8 +1685,8 @@ def main():
         # warped_img2 = warp(img2, flow_list[3][:, 2:4])
         # output = warped_img0 * mask_list[3] + warped_img2 * (1 - mask_list[3])
 
-        pm_weight = 0.1
-        lpips_weight = 0.25
+        pm_weight = 0.05
+        lpips_weight = 0.2
         x8_output = torch.nn.functional.interpolate(merged[0], scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_orig = torch.nn.functional.interpolate(img1, scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_lpips = torch.mean(loss_fn_alex(x8_output * 2 - 1, x8_orig * 2 - 1))
