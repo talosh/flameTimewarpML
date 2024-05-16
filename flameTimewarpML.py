1fa12357490143c9533a3f6931698243343dc861
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-16 20:24:58 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -289,6 +289,7 @@ class ApplyModelDialog():
         )
         # Window layout
         grid_layout = QtWidgets.QGridLayout()
+        grid_layout.setRowMinimumHeight(0, 30)
         grid_layout.setRowMinimumHeight(1, 30)
         grid_layout.setColumnMinimumWidth(2, 150)
         grid_layout.setColumnMinimumWidth(3, 150)
