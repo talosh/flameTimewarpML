f671acdf36b779e29c96c7fbafc654ba4d0860d8
Your Name <you@example.com>
2024-05-16 11:45:58 +0100

    test lpips

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1696,26 +1696,26 @@ def main():
         lpips_weight = 0.2
         x8_output = torch.nn.functional.interpolate(merged[0], scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
         x8_orig = torch.nn.functional.interpolate(img1, scale_factor= 1. / training_scale[0], mode="bilinear", align_corners=False)
-        x8_lpips = torch.mean(loss_fn_lpips.forward(x8_output * 2 - 1, x8_orig * 2 - 1))
+        # x8_lpips = torch.mean(loss_fn_lpips.forward(x8_output * 2 - 1, x8_orig * 2 - 1))
         # x8_lpips = torch.nn.functional.interpolate(x8_lpips, scale_factor = training_scale[0], mode="bilinear", align_corners=False)
         loss_x8 = pm_weight * criterion_huber(x8_output, x8_orig) # + lpips_weight * x8_lpips
 
         x4_output = torch.nn.functional.interpolate(merged[1], scale_factor= 1. / training_scale[1], mode="bilinear", align_corners=False)
         x4_orig = torch.nn.functional.interpolate(img1, scale_factor= 1. / training_scale[1], mode="bilinear", align_corners=False)
-        x4_lpips = torch.mean(loss_fn_lpips.forward(x4_output * 2 - 1, x4_orig * 2 - 1))
+        # x4_lpips = torch.mean(loss_fn_lpips.forward(x4_output * 2 - 1, x4_orig * 2 - 1))
         # x4_lpips = torch.nn.functional.interpolate(x4_lpips, scale_factor = training_scale[1], mode="bilinear", align_corners=False)
         loss_x4 = pm_weight * criterion_huber(x4_output, x4_orig) # + lpips_weight * x4_lpips
 
         x2_output = torch.nn.functional.interpolate(merged[2], scale_factor= 1. / training_scale[2], mode="bilinear", align_corners=False)
         x2_orig = torch.nn.functional.interpolate(img1, scale_factor= 1. / training_scale[2], mode="bilinear", align_corners=False)
-        x2_lpips = torch.mean(loss_fn_lpips.forward(x2_output * 2 - 1, x2_orig * 2 - 1))
+        # x2_lpips = torch.mean(loss_fn_lpips.forward(x2_output * 2 - 1, x2_orig * 2 - 1))
         # x2_lpips = torch.nn.functional.interpolate(x2_lpips, scale_factor = training_scale[2], mode="bilinear", align_corners=False)
         loss_x2 = pm_weight * criterion_huber(x2_output, x2_orig) # + lpips_weight * x2_lpips
 
         x1_output = merged[3]
         x1_orig = img1
-        x1_lipis = torch.mean(loss_fn_lpips.forward(x1_output * 2 - 1, x1_orig * 2 - 1))
-        loss_x1 = pm_weight * criterion_huber(x1_output, x1_orig) + lpips_weight * x1_lipis
+        # x1_lipis = torch.mean(loss_fn_lpips.forward(x1_output * 2 - 1, x1_orig * 2 - 1))
+        loss_x1 = pm_weight * criterion_huber(x1_output, x1_orig) + # lpips_weight * x1_lipis
 
         loss = 0.24 * loss_x8 + 0.24 * loss_x4 + 0.24 * loss_x2 + 0.28 * loss_x1
         loss.backward()
@@ -1723,11 +1723,12 @@ def main():
         loss_l1 = criterion_l1(restore_normalized_values(output), img1_orig)
         loss_l1_str = str(f'{loss_l1.item():.6f}')
 
-        loss_LPIPS_ = loss_fn_alex(restore_normalized_values(output) * 2 - 1, img1_orig * 2 - 1)
+        # loss_LPIPS_ = loss_fn_alex(restore_normalized_values(output) * 2 - 1, img1_orig * 2 - 1)
 
         epoch_loss.append(float(loss_l1.item()))
         steps_loss.append(float(loss_l1.item()))
-        lpips_list.append(float(torch.mean(loss_LPIPS_).item()))
+        # lpips_list.append(float(torch.mean(loss_LPIPS_).item()))
+        lpips_list.append(1.)
         psnr_list.append(float(psnr_torch(output, img1)))
 
         if len(epoch_loss) < 9999:
