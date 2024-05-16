032351296ccac84d0d62bb6f4484f53dee2410d9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-16 01:46:46 +0100

    update README.md and flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1535,6 +1535,17 @@ def main():
             param.requires_grad = False
 
         # '''
+        for param in flownet.block0.convblock[0].parameters():
+            param.requires_grad = True
+        for param in flownet.block1.convblock[0].parameters():
+            param.requires_grad = True
+        for param in flownet.block2.convblock[0].parameters():
+            param.requires_grad = True
+        for param in flownet.block3.convblock[0].parameters():
+            param.requires_grad = True
+        # '''
+
+        # '''
         for param in flownet.block0.convblock[-1].parameters():
             param.requires_grad = True
         for param in flownet.block1.convblock[-1].parameters():
