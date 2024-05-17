451f759ad431cabcee636802c0d35728fd45de79
Your Name <you@example.com>
2024-05-17 16:12:25 +0100

    wbal

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1811,7 +1811,7 @@ def main():
         # warped_img2 = warp(img2, flow_list[3][:, 2:4])
         # output = warped_img0 * mask_list[3] + warped_img2 * (1 - mask_list[3])
 
-        pm_weight = 0.05
+        pm_weight = 0.04
         lpips_weight = 4e-4
         # self.vgg(merged[3], gt).mean() - self.ss(merged[3], gt) * 0.1
 
