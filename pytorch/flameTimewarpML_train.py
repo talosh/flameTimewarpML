6d7a5d7224919448b4f777b0ad1aafab3a08a917
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-17 07:30:56 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1185,7 +1185,7 @@ def gaussian(window_size, sigma):
 def create_window(window_size, channel):
     _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
-    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    window = torch.autograd.Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
     return window
 
 def _ssim(img1, img2, window, window_size, channel, size_average = True, stride=None):
