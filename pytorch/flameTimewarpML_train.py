05ba20edf576859c82e4009dbc16d0eb7cd00af8
Your Name <you@example.com>
2024-05-18 14:46:56 +0100

    huber delta 0.001

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1430,7 +1430,7 @@ def main():
 
     criterion_mse = torch.nn.MSELoss()
     criterion_l1 = torch.nn.L1Loss()
-    criterion_huber = torch.nn.HuberLoss(delta=0.004)
+    criterion_huber = torch.nn.HuberLoss(delta=0.001)
 
     weight_decay = 10 ** (0.07 * args.generalize - 9) if args.generalize > 1 else 1e-9
 
