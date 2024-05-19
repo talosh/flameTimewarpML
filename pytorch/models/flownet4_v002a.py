437048cb175b65f096dc6b5ae5c35d4650e44ba4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-19 19:34:11 +0100

    update flownet4_v002a.py

--- pytorch/models/flownet4_v002a.py
+++ pytorch/models/flownet4_v002a.py
@@ -327,15 +327,15 @@ class Model:
     @staticmethod
     def get_info():
         info = {
-            'name': 'Flownet4_v002',
-            'file': 'flownet4_v002.py',
+            'name': 'Flownet4_v002a',
+            'file': 'flownet4_v002a.py',
             'ratio_support': True
         }
         return info
 
     @staticmethod
     def get_name():
-        return 'TWML_Flownet_v002'
+        return 'TWML_Flownet_v002a'
 
     @staticmethod
     def input_channels(model_state_dict):
