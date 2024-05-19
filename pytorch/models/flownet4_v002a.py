fca458a874b8b7ff86eb72ba67946c1310110bba
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-19 13:01:12 +0100

    update flownet4_v001b.py and flownet4_v002a.py

--- pytorch/models/flownet4_v002a.py
+++ pytorch/models/flownet4_v002a.py
@@ -3,6 +3,7 @@ class Model:
         if torch is None:
             import torch
         Module = torch.nn.Module
+        backwarp_tenGrid = {}
 
         def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
             return torch.nn.Sequential(
@@ -20,7 +21,7 @@ class Model:
                 # torch.nn.SELU(inplace = True)
             )
 
-        '''
+        # '''
         def warp(tenInput, tenFlow):
             backwarp_tenGrid = {}
             k = (str(tenFlow.device), str(tenFlow.size()))
@@ -32,9 +33,9 @@ class Model:
 
             g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
             return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
-        '''
-
         # '''
+
+        '''
         def warp(tenInput, tenFlow):
             input_device = tenInput.device
             input_dtype = tenInput.dtype
@@ -42,7 +43,6 @@ class Model:
                 tenInput = tenInput.detach().to(device=torch.device('cpu'), dtype=torch.float32)
                 tenFlow = tenFlow.detach().to(device=torch.device('cpu'), dtype=torch.float32)
 
-            backwarp_tenGrid = {}
             k = (str(tenFlow.device), str(tenFlow.size()))
             if k not in backwarp_tenGrid:
                 tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
@@ -60,7 +60,7 @@ class Model:
                 )
 
             return result.detach().to(device=input_device, dtype=input_dtype)
-        # '''
+        '''
 
 
         class Conv2d(Module):
