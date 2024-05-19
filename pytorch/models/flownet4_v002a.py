a1afc227edb0f3d20bcd7bc14d04f64fc37ba657
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-19 18:21:18 +0100

    update flownet4_v002a.py

--- pytorch/models/flownet4_v002a.py
+++ pytorch/models/flownet4_v002a.py
@@ -129,9 +129,15 @@ class Model:
                     ResConv(c),
                     ResConv(c),
                 )
-                self.lastconv3 = torch.nn.Sequential(
+                self.lastconv = torch.nn.Sequential(
+                    torch.nn.ConvTranspose2d(c, 4*4, 4, 2, 1),
+                    torch.nn.PixelShuffle(2)
+                )
+                self.lastconv2 = torch.nn.Sequential(
                     torch.nn.ConvTranspose2d(c, c//2, 4, 2, 1),
-                    torch.nn.ConvTranspose2d(c//2, 5, 4, 2, 1),
+                    conv(c//2, c//2, 3, 1, 1),
+                    torch.nn.ConvTranspose2d(c//2, c//4, 4, 2, 1),
+                    conv(c//4, 1, 3, 1, 1),
                 )
 
             def forward(self, img0, img1, f0, f1, timestep, mask, flow, scale=1):
@@ -144,10 +150,10 @@ class Model:
                     x = torch.cat((x, mask, flow), 1)
                 feat = self.conv0(x)
                 feat = self.convblock(feat)
-                tmp = self.lastconv2(feat)
-                tmp = torch.nn.functional.interpolate(tmp, scale_factor=scale, mode="bilinear", align_corners=False)
-                flow = tmp[:, :4] * scale
-                mask = tmp[:, 4:5]
+                flow = self.lastconv(feat)
+                flow = torch.nn.functional.interpolate(flow, scale_factor=scale, mode="bilinear", align_corners=False) * scale
+                mask = self.lastconv2(feat)
+                flow = torch.nn.functional.interpolate(mask, scale_factor=scale, mode="bilinear", align_corners=False)
                 return flow, mask
 
         class FlownetCas(Module):
