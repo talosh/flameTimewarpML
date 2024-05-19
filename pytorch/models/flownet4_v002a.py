97466e1f6e5717e89c33172c5ddb1369e022aef3
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-19 21:07:25 +0100

    update flownet4_v002a.py

--- pytorch/models/flownet4_v002a.py
+++ pytorch/models/flownet4_v002a.py
@@ -193,9 +193,10 @@ class Model:
                 feat = self.conv0(x)
                 feat = self.convblock(feat)
                 tmp = self.lastconv(feat)
+                tmp [:, 4:5] += (torch.sigmoid(self.lastconv2(feat)) * 2 - 1)
                 tmp = torch.nn.functional.interpolate(tmp, scale_factor=scale, mode="bilinear", align_corners=False)
                 flow = tmp[:, :4] * scale
-                mask = tmp [:, 4:5] + (torch.sigmoid(self.lastconv2(feat)) * 2 - 1)
+                mask = tmp [:, 4:5]
                 return flow, mask
 
         class FlownetCas(Module):
