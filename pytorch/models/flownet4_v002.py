dd84ae42f2c2b4bc4b0295499e3a6f75b12f936f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-19 13:36:13 +0100

    update flownet4_v002.py

--- pytorch/models/flownet4_v002.py
+++ pytorch/models/flownet4_v002.py
@@ -23,6 +23,7 @@ class Model:
 
         '''
         def warp(tenInput, tenFlow):
+            backwarp_tenGrid = {}
             k = (str(tenFlow.device), str(tenFlow.size()))
             if k not in backwarp_tenGrid:
                 tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
@@ -34,7 +35,6 @@ class Model:
             return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
         '''
 
-        # '''
         def warp(tenInput, tenFlow):
             input_device = tenInput.device
             input_dtype = tenInput.dtype
@@ -42,6 +42,7 @@ class Model:
                 tenInput = tenInput.detach().to(device=torch.device('cpu'), dtype=torch.float32)
                 tenFlow = tenFlow.detach().to(device=torch.device('cpu'), dtype=torch.float32)
 
+            backwarp_tenGrid = {}
             k = (str(tenFlow.device), str(tenFlow.size()))
             if k not in backwarp_tenGrid:
                 tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
@@ -59,7 +60,6 @@ class Model:
                 )
 
             return result.detach().to(device=input_device, dtype=input_dtype)
-        # '''
 
         class Head(Module):
             def __init__(self):
