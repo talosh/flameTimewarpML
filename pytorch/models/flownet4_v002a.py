8c8400b8454ca6f47584827a605e14f5ee5b0b6f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-20 07:22:56 +0100

    update flameTimewarpML_train.py and flownet4_v002a.py

--- pytorch/models/flownet4_v002a.py
+++ pytorch/models/flownet4_v002a.py
@@ -191,11 +191,10 @@ class Model:
                 feat = self.conv0(x)
                 feat = self.convblock(feat)
                 tmp = self.lastconv(feat)
-                # tmp [:, 4:5] += torch.sigmoid()
+                tmp[:, 4:5] += self.lastconv2(feat)
                 tmp = torch.nn.functional.interpolate(tmp, scale_factor=scale, mode="bilinear", align_corners=False)
                 flow = tmp[:, :4] * scale
-                # mask = tmp [:, 4:5]
-                mask = tmp [:, 4:5] + self.lastconv2(feat)
+                mask = tmp [:, 4:5]
                 return flow, mask
 
         class FlownetCas(Module):
