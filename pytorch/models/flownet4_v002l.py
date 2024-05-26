6d5b23a3f13d10bf954e72aad182b8d6d4f2afe8
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-26 01:27:31 +0100

    update flownet4_v001e.py and flownet4_v002l.py

--- pytorch/models/flownet4_v002l.py
+++ pytorch/models/flownet4_v002l.py
@@ -112,7 +112,9 @@ class Model:
                     torch.nn.ConvTranspose2d(c, c, 4, 2, 1),
                     torch.nn.Conv2d(c, c, 3, 1, 1, groups=c, bias=False, padding_mode = 'reflect'),
                     torch.nn.Conv2d(c, c, kernel_size=1, stride=1, padding=0, bias=True),
-                    torch.nn.ConvTranspose2d(c, 6, 4, 2, 1)
+                    torch.nn.ConvTranspose2d(c, c//2, 4, 2, 1)
+                    torch.nn.Conv2d(c//2, c//2, 3, 1, 1, groups=c//2, bias=False, padding_mode = 'reflect'),
+                    torch.nn.Conv2d(c//2, 6, kernel_size=1, stride=1, padding=0, bias=True),
                     # torch.nn.PixelShuffle(2)
                 )
 
