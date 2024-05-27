02c15f2b14090945cee186266be0da1e43726c20
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-27 14:27:24 +0100

    update flownet4_v003a.py

--- pytorch/models/flownet4_v003a.py
+++ pytorch/models/flownet4_v003a.py
@@ -62,9 +62,9 @@ class Model:
         class Head(Module):
             def __init__(self):
                 super(Head, self).__init__()
-                self.cnn0 = torch.nn.Conv2d(3, 32, 3, 2, 1)
-                self.cnn1 = torch.nn.Conv2d(32, 32, 3, 1, 1)
-                self.cnn2 = torch.nn.Conv2d(32, 32, 3, 1, 1)
+                self.cnn0 = torch.nn.Conv2d(3, 32, 3, 2, 1, padding_mode = 'reflect')
+                self.cnn1 = torch.nn.Conv2d(32, 32, 3, 1, 1, padding_mode = 'reflect')
+                self.cnn2 = torch.nn.Conv2d(32, 32, 3, 1, 1, padding_mode = 'reflect')
                 self.cnn3 = torch.nn.ConvTranspose2d(32, 8, 4, 2, 1)
                 self.relu = torch.nn.LeakyReLU(0.2, True)
 
@@ -100,9 +100,9 @@ class Model:
         class Encode01(Module):
             def __init__(self):
                 super(Encode01, self).__init__()
-                self.cnn0 = torch.nn.Conv2d(3 + 8, 48, 3, 2, 1)
-                self.cnn1 = torch.nn.Conv2d(48, 48, 3, 1, 1)
-                self.cnn2 = torch.nn.Conv2d(48, 48, 3, 1, 1)
+                self.cnn0 = torch.nn.Conv2d(3 + 8, 48, 3, 2, 1, padding_mode = 'reflect')
+                self.cnn1 = torch.nn.Conv2d(48, 48, 3, 1, 1, padding_mode = 'reflect')
+                self.cnn2 = torch.nn.Conv2d(48, 48, 3, 1, 1, padding_mode = 'reflect')
                 self.cnn3 = torch.nn.ConvTranspose2d(48, 8, 4, 2, 1)
                 self.relu = torch.nn.LeakyReLU(0.2, True)
                 self.cnn4 = torch.nn.Conv2d(16, 8, 1, 1, 0)
