e5a53904143696959b118ff654d8e886f724dc8c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-05-27 14:26:08 +0100

    update flownet4_v003a.py

--- pytorch/models/flownet4_v003a.py
+++ pytorch/models/flownet4_v003a.py
@@ -105,7 +105,7 @@ class Model:
                 self.cnn2 = torch.nn.Conv2d(48, 48, 3, 1, 1)
                 self.cnn3 = torch.nn.ConvTranspose2d(48, 8, 4, 2, 1)
                 self.relu = torch.nn.LeakyReLU(0.2, True)
-                self.cnn4 = torch.nn.Conv2d(16, 8, 1, 1, 1)
+                self.cnn4 = torch.nn.Conv2d(16, 8, 1, 1, 0)
 
                 torch.nn.init.kaiming_normal_(self.cnn0.weight, mode='fan_in', nonlinearity='relu')
                 self.cnn0.weight.data *= 1e-2
