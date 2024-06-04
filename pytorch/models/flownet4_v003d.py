66e24e6c043d59b3121d933103dd8bb5d67a269e
Your Name <you@example.com>
2024-06-04 17:22:41 +0100

    v003d fix

--- pytorch/models/flownet4_v003d.py
+++ pytorch/models/flownet4_v003d.py
@@ -206,7 +206,7 @@ class Model:
                     mask = torch.nn.functional.interpolate(mask, scale_factor= 1. / scale, mode="bilinear", align_corners=False)
                     flow = torch.nn.functional.interpolate(flow, scale_factor= 1. / scale, mode="bilinear", align_corners=False) * 1. / scale
                     timestep = (img0[:, :1].clone() * 0 + 1) * timestep
-                    x = torch.cat((img0, img1, f0, f1, timestep, mask, flow), 1)
+                    x = torch.cat((img0_warped, img1_warped, f0_warped, f1_warped, timestep, mask, flow), 1)
 
                 feat = self.conv0(x)
                 feat = self.convblock(feat)
