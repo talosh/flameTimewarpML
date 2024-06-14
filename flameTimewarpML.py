1c6ca4eb0a57b0f279b95f7bbf342acb2adab5e1
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 00:02:27 +0100

    update flameTimewarpML.py

--- flameTimewarpML.py
+++ flameTimewarpML.py
@@ -411,6 +411,7 @@ class ApplyModelDialog():
 
         def open_dest_model_browser():
             self.window.hide()
+            print (f'res_model_path {res_model_path}')
 
             if not os.path.isfile(res_model_path):
                 import shutil
