af45cb24070855606e6bd7a61950590d4dceb261
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 18:18:04 +0100

    update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -1509,7 +1509,7 @@ def main():
             self.text_edit.setStyleSheet("""
                 QTextEdit {
                     color: rgb(188, 188, 188); 
-                    background-color: #294929;
+                    background-color: #45676F;
                     border: 1px solid #474747;
                 }
             """)
