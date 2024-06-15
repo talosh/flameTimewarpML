1cd924c6da167b3debb5a07f72acccee0880ba39
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 19:33:32 +0100

    update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -587,6 +587,12 @@ def main():
                     self.text_edit.moveCursor(QTextCursor.End)
                     self.text_edit.textCursor().deletePreviousChar()  # Remove newline left after text removal
                 self.last_progress_line = text
+            elif '\x1b[1A' in text:
+                self.text_edit.moveCursor(QTextCursor.Up)
+                self.text_edit.moveCursor(QTextCursor.StartOfLine, QTextCursor.KeepAnchor)
+                self.text_edit.textCursor().removeSelectedText()
+                self.text_edit.moveCursor(QTextCursor.End)
+                self.text_edit.textCursor().deletePreviousChar()
             else:
                 pass
 
