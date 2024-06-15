8eff73b16272da2a541c1500d5792e19240dd7d4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 21:14:23 +0100

    update flameTimewarpML_finetune.py and flameTimewarpML_train.py update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -1664,7 +1664,7 @@ def main():
                 hours = int((epoch_time % (24 * 3600)) // 3600)
                 minutes = int((epoch_time % 3600) // 60)
 
-                clear_lines(2)
+                clear_lines(1)
                 print (f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Time:{data_time_str} + {train_time_str}, Batch [Step: {batch_idx+1}, Sample: {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss L1: {loss_l1_str}')
                 print(f'\r[Epoch] Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f} LPIPS: {lpips_val:.4f}')
 
