e661695053c5aab364b5eb5ce3d0d29c896964c2
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 22:28:54 +0100

    update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -2058,8 +2058,10 @@ def main():
             cursor.movePosition(QTextCursor.End)
             cursor.insertText(text)  # Insert the text at the end
             self.text_edit.setTextCursor(cursor)
-            self.text_edit.setUpdatesEnabled(True)
             self.text_edit.ensureCursorVisible()
+            
+            self.text_edit.setUpdatesEnabled(True)
+
 
         def keyPressEvent(self, event):
             if event.key() == Qt.Key_Control:
