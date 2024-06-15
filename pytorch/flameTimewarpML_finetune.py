24a73111c373450a304ef6c33ca2ce614f997599
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 20:34:22 +0100

    update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -1619,7 +1619,8 @@ def main():
             self.text_edit.ensureCursorVisible()
 
         def keyPressEvent(self, event):
-            print ('key press event')      
+            print ('key press event')
+            self.close()
             # Check if Ctrl+C was pressed
             if event.key() == Qt.Key_C and event.modifiers():
                 self.worker.graceful_exit()
