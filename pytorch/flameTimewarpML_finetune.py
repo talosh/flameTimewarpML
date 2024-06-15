2aa63dd586a08231dac30392a6024c05415df184
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 22:13:19 +0100

    update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -2038,9 +2038,9 @@ def main():
             elif '\x1b[1A' in text:
                 self.text_edit.moveCursor(QTextCursor.Up)
                 self.text_edit.moveCursor(QTextCursor.StartOfLine, QTextCursor.KeepAnchor)
-                # self.text_edit.textCursor().removeSelectedText()
-                # self.text_edit.moveCursor(QTextCursor.End)
-                # self.text_edit.textCursor().deletePreviousChar()
+                self.text_edit.textCursor().removeSelectedText()
+                self.text_edit.moveCursor(QTextCursor.End)
+                self.text_edit.textCursor().deletePreviousChar()
                 text = ''
             elif '\x1b[2K' in text:
                 self.text_edit.moveCursor(QTextCursor.StartOfLine, QTextCursor.KeepAnchor)
