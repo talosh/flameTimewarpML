e2a9542bb47c8aaab3fa63983b2a2098ab5c03bf
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-15 22:50:56 +0100

    update flameTimewarpML_finetune.py

--- pytorch/flameTimewarpML_finetune.py
+++ pytorch/flameTimewarpML_finetune.py
@@ -454,7 +454,7 @@ def main():
                 ERASE_LINE = '\x1b[2K'
                 for _ in range(n):
                     sys.stdout.write(CURSOR_UP_ONE)
-                    # sys.stdout.write(ERASE_LINE)
+                    sys.stdout.write(ERASE_LINE)
 
             def find_and_import_model(models_dir='models', base_name=None, model_name=None, model_file=None):
                 """
@@ -2047,10 +2047,10 @@ def main():
                 self.last_progress_line = text
             elif '\x1b[1A' in text:
                 self.text_edit.moveCursor(QTextCursor.Up)
-                self.text_edit.moveCursor(QTextCursor.StartOfLine, QTextCursor.KeepAnchor)
-                self.text_edit.textCursor().removeSelectedText()
-                self.text_edit.moveCursor(QTextCursor.End)
-                self.text_edit.textCursor().deletePreviousChar()
+                # self.text_edit.moveCursor(QTextCursor.StartOfLine, QTextCursor.KeepAnchor)
+                # self.text_edit.textCursor().removeSelectedText()
+                # self.text_edit.moveCursor(QTextCursor.End)
+                # self.text_edit.textCursor().deletePreviousChar()
                 text = ''
             elif '\x1b[2K' in text:
                 self.text_edit.moveCursor(QTextCursor.StartOfLine, QTextCursor.KeepAnchor)
