3e0eb5fa6224de25f37d40e0a5332e45c3409acf
Your Name <you@example.com>
2024-06-19 17:32:32 +0100

    scale augm

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1977,9 +1977,9 @@ def main():
         else:
             training_scale = [8, 4, 2, 1]
             
-        if random.uniform(0, 1) < 0.22:
+        if random.uniform(0, 1) < 0.165:
             training_scale = [x * 2 for x in training_scale]
-        elif random.uniform(0, 1) < 0.44:
+        elif random.uniform(0, 1) < 0.33:
             training_scale = [1/2 if x == 1 else x / 2 for x in training_scale]
 
         flownet.train()
