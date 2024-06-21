4f7f9c4c4b56477d3158897fb7ccb9a610a2a747
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-06-21 07:23:04 +0100

    update flownet4_v001.py, flownet4_v001a.py and flownet4_v001eg.py

--- pytorch/models/flownet4_v001a.py
+++ pytorch/models/flownet4_v001a.py
@@ -135,7 +135,7 @@ class Model:
                 self.block3 = Flownet(8+4+16, c=64)
                 self.encode = Head()
 
-            def forward(self, img0, img1, timestep=0.5, scale=[8, 4, 2, 1], iterations=1):
+            def forward(self, img0, img1, timestep=0.5, scale=[16, 8, 4, 1], iterations=1):
                 img0 = img0
                 img1 = img1
                 f0 = self.encode(img0)
