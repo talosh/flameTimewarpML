125b7011ef4036b66320d6e4917d108a4320eff9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-09-10 11:13:54 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2275,10 +2275,14 @@ def main():
                         pprint (f'\nerror while reading eval images: {e}\n{description}\n\n')
                 read_eval_image_queue.put(None)
 
+            print ('starting eval read thread')
+
             read_eval_image_queue = queue.Queue(maxsize=8)
             read_eval_thread = threading.Thread(target=read_eval_images, args=(read_eval_image_queue, descriptions))
             read_eval_thread.daemon = True
             read_eval_thread.start()
+
+            print ('eval read thread started')
 
             eval_loss = []
             eval_psnr = []
@@ -2292,6 +2296,7 @@ def main():
             with torch.no_grad():
                 # for ev_item_index, description in enumerate(descriptions):
                 description = read_eval_image_queue.get()
+                print (f'description read: {description.keys()}')
                 while description is not None:
                     ev_item_index = description['ev_item_index']
                     
