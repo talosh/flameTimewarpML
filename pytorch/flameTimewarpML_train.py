0c8022e328f1f9b948ca32d28a8397cd08a4d5b8
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-09-16 10:51:17 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1993,7 +1993,7 @@ def main():
             [1, 1, 1, 1],
         ]
 
-        if random.uniform(0, 1) < 0.69:
+        if random.uniform(0, 1) < 0.44:
             training_scale = random_scales[random.randint(0, len(random_scales) - 1)]
         else:
             training_scale = [8, 4, 2, 1] # [16, 8, 4, 1]
