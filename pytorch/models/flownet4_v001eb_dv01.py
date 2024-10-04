5e6ff64e042b780665ac2de152e60d1679ea3745
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-04 13:20:00 +0100

    update flownet4_v001eb_dv01.py

--- pytorch/models/flownet4_v001eb_dv01.py
+++ pytorch/models/flownet4_v001eb_dv01.py
@@ -8,7 +8,7 @@ class Model:
     info = {
         'name': 'Flownet4_v001eb_dv01',
         'file': 'flownet4_v001eb_dv01.py',
-        'padding': 128,
+        'padding': 192,
         'ratio_support': True
     }
 
@@ -174,7 +174,7 @@ class Model:
                 self.block0 = Flownet(7+16, c=192)
                 self.block1 = Flownet(8+4+16, c=128)
                 self.block2 = Flownet(8+4+16, c=96)
-                self.block3 = Flownet(8+4+16, c=64)
+                self.block3 = Flownet(8+4+16, c=48)
                 self.encode = Head()
 
             def forward(self, img0, img1, timestep=0.5, scale=[16, 8, 4, 1], iterations=1):
