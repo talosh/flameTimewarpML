0f7f3e3d950b1ac5b82ebc2e6d52b8ffe95a992f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-05 23:54:58 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1496,7 +1496,7 @@ def main():
         hours = int((epoch_time % (24 * 3600)) // 3600)
         minutes = int((epoch_time % 3600) // 60)
 
-        clear_lines(2)
+        clear_lines(1)
         print (f'\r[Epoch {(epoch + 1):04} Step {step:08} - {days:02}d {hours:02}:{minutes:02}], Time: {data_time_str}+{train_time_str}, Batch [{batch_idx+1}, Sample: {idx+1} / {len(dataset)}], Lr: {current_lr_str}, Loss L1: {loss_l1_str}')
         # if len(epoch_loss) < 9999:
         #    print(f'\r[Epoch] Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f} LPIPS: {lpips_val:.4f}')
