6b4039599c61d3243c1432bd39ef837ababb6d10
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-06 17:09:07 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1446,6 +1446,7 @@ def main():
         data_time_str = str(f'{data_time:.2f}')
         time_stamp = time.time()
 
+        '''
         # train here
         img0 = img0.to(device = device, dtype = torch.float32, non_blocking=True)
         img1 = img1.to(device = device, dtype = torch.float32, non_blocking=True)
@@ -1453,6 +1454,7 @@ def main():
 
         for i in range(img0.shape[0]):
             img0[i], img1[i], img2[i] = augment_images(img0[i], img1[i], img2[i], args.generalize, args.acescc)
+        '''
 
         current_lr_str = '0'
         loss_l1_str = '0'
