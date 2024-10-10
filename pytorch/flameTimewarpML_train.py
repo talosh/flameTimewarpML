3f6a5c5729999bf3bd8df9035c00a36913367193
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-10 18:01:31 +0100

    update flameTimewarpML_train.py and flownet4_v001eb_dv01.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1734,13 +1734,13 @@ def main():
             try:
                 write_data = write_image_queue.get_nowait()
                 preview_index = write_data.get('preview_index', 0)
-                write_exr(write_data['sample_source1'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_incomng.exr'), half_float = True)
-                write_exr(write_data['sample_source2'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_outgoing.exr'), half_float = True)
-                write_exr(write_data['sample_target'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_target.exr'), half_float = True)
-                write_exr(write_data['sample_output'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_output.exr'), half_float = True)
-                write_exr(write_data['sample_output_diff'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_output_diff.exr'), half_float = True)
-                write_exr(write_data['sample_output_mask'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_output_mask.exr'), half_float = True)
-                write_exr(write_data['sample_output_conf'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_output_conf.exr'), half_float = True)
+                write_exr(write_data['sample_source1'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_A_incomng.exr'), half_float = True)
+                write_exr(write_data['sample_source2'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_B_outgoing.exr'), half_float = True)
+                write_exr(write_data['sample_target'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_C_target.exr'), half_float = True)
+                write_exr(write_data['sample_output'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_D_output.exr'), half_float = True)
+                write_exr(write_data['sample_output_diff'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}__E_output_diff.exr'), half_float = True)
+                write_exr(write_data['sample_output_mask'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_F_output_mask.exr'), half_float = True)
+                write_exr(write_data['sample_output_conf'].astype(np.float16), os.path.join(write_data['preview_folder'], f'{preview_index:02}_G_output_conf.exr'), half_float = True)
                 del write_data
             except:
             # except queue.Empty:
