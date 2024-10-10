9a0a644725f05d04aa6eeacfb84f651f5c0555dd
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-10 23:42:29 +0100

    update flownet4_v001eb_dv01.py

--- pytorch/models/flownet4_v001eb_dv01.py
+++ pytorch/models/flownet4_v001eb_dv01.py
@@ -272,9 +272,6 @@ class Model:
                 pw = ((w - 1) // pvalue + 1) * pvalue
                 padding = (0, pw - w, 0, ph - h)
                 
-                print (f'\n\nph {ph}')
-                print (f'pw{pw}\n\n')
-
                 if flow is None:
                     x = torch.cat((img0, img1, f0, f1, timestep), 1)
                     x = torch.nn.functional.pad(x, padding, mode='replicate')
