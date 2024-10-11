bd578b1b9e698e6493bfb61d3bc8f73ce8419131
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-11 17:16:12 +0100

    update flownet4_v001eb_dv01.py

--- pytorch/models/flownet4_v001eb_dv01.py
+++ pytorch/models/flownet4_v001eb_dv01.py
@@ -333,6 +333,7 @@ class Model:
                 scale[1] = scale[3]
                 scale[2] = 1
                 '''
+                scale[0] = 1
 
                 flow, mask, conf = self.block0(
                     img0, 
@@ -349,6 +350,16 @@ class Model:
                 mask_list[0] = torch.sigmoid(mask)
                 conf_list[0] = torch.sigmoid(conf)
                 merged[0] = warp(img0, flow[:, :2]) * mask_list[0] + warp(img1, flow[:, 2:4]) * (1 - mask_list[0])
+
+                # '''
+                # step training
+                flow_list[4] = flow_list[0]
+                mask_list[4] = mask_list[0]
+                conf_list[4] = conf_list[0]
+                merged[4] = merged[0]
+
+                return flow_list, mask_list, conf_list, merged
+                # '''
 
                 # refine step 1
                 flow_d, mask, conf_d = self.block1(
