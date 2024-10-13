0cc672c0d1df42f82bb7929a8a6c2e1a38ab6096
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-13 23:35:15 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2371,7 +2371,7 @@ def main():
         min_l1 = min(min_l1, float(loss_l1.item()))
         max_l1 = max(max_l1, float(loss_l1.item()))
         avg_l1 = float(loss_l1.item()) if avg_l1 == 0 else (avg_l1 * (step - 1) + float(loss_l1.item())) / step
-        avg_lpips = (avg_lpips * (step - 1) + float(torch.mean(loss_LPIPS_).item())) / step
+        avg_lpips = float(torch.mean(loss_LPIPS_).item()) if avg_lpips == 0 else (avg_lpips * (step - 1) + float(torch.mean(loss_LPIPS_).item())) / step
         avg_pnsr = (avg_pnsr * (step - 1) + float(psnr_torch(output, img1))) / step
 
         loss.backward()
