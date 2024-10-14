78bdc313e1ea66afac515948a3f3bec3d70d439c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-14 10:44:19 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2117,11 +2117,13 @@ def main():
     print (f'flownet.module.block0.attn_deep.spatial_scale {flownet.module.block0.attn_deep.spatial_scale.data}')
     print (f'flownet.module.block0.attn_deep.spatial_offset {flownet.module.block0.attn_deep.spatial_offset.data}')
 
+    print ()
     print (f'flownet.module.block1.attn.channel_scale {flownet.module.block1.attn.channel_scale.data}')
     print (f'flownet.module.block1.attn.channel_offset {flownet.module.block1.attn.channel_offset.data}')
     print (f'flownet.module.block1.attn.spatial_scale {flownet.module.block1.attn.spatial_scale.data}')
     print (f'flownet.module.block1.attn.spatial_offset {flownet.module.block1.attn.spatial_offset.data}')
 
+    print ()
     print (f'flownet.module.block2.attn.channel_scale {flownet.module.block2.attn.channel_scale.data}')
     print (f'flownet.module.block2.attn.channel_offset {flownet.module.block2.attn.channel_offset.data}')
     print (f'flownet.module.block2.attn.spatial_scale {flownet.module.block2.attn.spatial_scale.data}')
