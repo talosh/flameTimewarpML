33d74ba4f58d077a2c111ddfa58b6b417c3529ce
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-16 19:10:21 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -830,9 +830,9 @@ def get_dataset(
             for index in range(self.batch_size):
                 img0, img1, img2 = self.crop(src_img0, src_img1, src_img2, self.h, self.w)
 
-                img0 = torch.from_numpy(img0).to(device = device, dtype = torch.float32)
-                img1 = torch.from_numpy(img1).to(device = device, dtype = torch.float32)
-                img2 = torch.from_numpy(img2).to(device = device, dtype = torch.float32)
+                img0 = torch.from_numpy(img0).to(device = 'cpu', dtype = torch.float32)
+                img1 = torch.from_numpy(img1).to(device = 'cpu', dtype = torch.float32)
+                img2 = torch.from_numpy(img2).to(device = 'cpu', dtype = torch.float32)
 
                 img0 = img0.permute(2, 0, 1)
                 img1 = img1.permute(2, 0, 1)
