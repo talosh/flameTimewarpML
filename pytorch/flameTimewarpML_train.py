926797bcaa216dcda32a4492b1444f2d7fb8545a
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-16 11:04:58 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -431,14 +431,12 @@ def read_image_file(file_path, header_only = False):
     if inp :
         spec = inp.spec()
         result['spec'] = spec
-        del spec
         if not header_only:
             channels = spec.nchannels
             img_data = inp.read_image(0, 0, 0, channels).transpose(1, 0, 2)
             result['image_data'] = np.ascontiguousarray(img_data)
             del img_data
         inp.close()
-        del inp
     return result
 
 def get_dataset(
