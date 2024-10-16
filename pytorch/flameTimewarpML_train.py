c4d5368a2405650a7927811fa9f1c8b818ea074e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-16 19:15:20 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -655,9 +655,13 @@ def get_dataset(
                         img1 = torch.nn.functional.interpolate(img1, size=(new_h, new_w), mode='bilinear', align_corners=False)
                         img2 = torch.nn.functional.interpolate(img2, size=(new_h, new_w), mode='bilinear', align_corners=False)
 
-                        train_data['start'] = img0.squeeze(0).permute(1, 2, 0)
-                        train_data['gt'] = img1.squeeze(0).permute(1, 2, 0)
-                        train_data['end'] = img2.squeeze(0).permute(1, 2, 0)
+                        img0 = img0.squeeze(0).permute(1, 2, 0)
+                        img1 = img1.squeeze(0).permute(1, 2, 0)
+                        img2 = img2.squeeze(0).permute(1, 2, 0)
+
+                        train_data['start'] = img0
+                        train_data['gt'] = img1
+                        train_data['end'] = img2
                         train_data['ratio'] = description['ratio']
                         train_data['h'] = description['h']
                         train_data['w'] = description['w']
