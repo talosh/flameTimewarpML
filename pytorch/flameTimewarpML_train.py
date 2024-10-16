fe6a1ddcfcb6b9eb1ef6d5a0d9d8f9ad6cb26fe9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-16 19:11:28 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -660,8 +660,8 @@ def get_dataset(
                         img2 = img0.squeeze(0).permute(1, 2, 0)
 
                         img0 = img0.numpy(force = True)
-                        img1 = img0.numpy(force = True)
-                        img2 = img0.numpy(force = True)
+                        img1 = img1.numpy(force = True)
+                        img2 = img2.numpy(force = True)
 
                         train_data['start'] = img0
                         train_data['gt'] = img1
