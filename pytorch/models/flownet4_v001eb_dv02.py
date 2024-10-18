874082a3f3603089822ff2238cb753971515283e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-18 18:20:16 +0100

    update flownet4_v001eb_dv02.py

--- pytorch/models/flownet4_v001eb_dv02.py
+++ pytorch/models/flownet4_v001eb_dv02.py
@@ -289,14 +289,6 @@ class Model:
                     ResConv(c),
                     ResConv(c),
                 )
-                '''
-                self.lastconv = torch.nn.Sequential(
-                    torch.nn.ConvTranspose2d(c*2, c, 6, 2, 2),
-                    torch.nn.Conv2d(c, c, kernel_size=1, stride=1, padding=0, bias=True),
-                    torch.nn.ConvTranspose2d(c, c, 4, 2, 1),
-                    torch.nn.Conv2d(c, 6, kernel_size=1, stride=1, padding=0, bias=True),
-                )
-                '''
                 self.lastconv = torch.nn.Sequential(
                     torch.nn.ConvTranspose2d(c, 4*6, 4, 2, 1),
                     torch.nn.PixelShuffle(2)
