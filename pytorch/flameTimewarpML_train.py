5c6bc6c714bc700b5e9b0eaf3fd87425d7907bd5
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-18 23:34:12 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2216,7 +2216,6 @@ def main():
             print(f'\nSaving current state to {current_state_dict["trained_model_path"]}...')
             print (f'Epoch: {current_state_dict["epoch"]}, Step: {current_state_dict["step"]:11}')
             torch.save(current_state_dict, current_state_dict['trained_model_path'])
-            time.sleep(1)
             signal.signal(signum, signal.SIG_DFL)
             os.kill(os.getpid(), signal.SIGINT)
         return graceful_exit
