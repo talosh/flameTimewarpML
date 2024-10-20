175f3c241d3a334b3c79ec35a6ab434136cbe9eb
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-21 00:25:14 +0100

    update flownet4_v001eb_dv04.py

--- pytorch/models/flownet4_v001eb_dv04.py
+++ pytorch/models/flownet4_v001eb_dv04.py
@@ -164,7 +164,9 @@ class Model:
             def __init__(self, c, dilation=1):
                 super().__init__()
                 self.conv = torch.nn.Conv2d(c, c, 3, 1, dilation, dilation = dilation, groups = 1, padding_mode = 'replicate', bias=True)
-                self.beta = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)    
+                self.conv1 = torch.nn.Conv2d(c, c, 3, 1, dilation, dilation = dilation, groups = 1, padding_mode = 'replicate', bias=True)
+                self.beta = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
+                self.gamma = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)    
                 self.relu = torch.nn.LeakyReLU(0.2, True)
 
                 torch.nn.init.kaiming_normal_(self.conv.weight, mode='fan_in', nonlinearity='relu')
@@ -173,7 +175,7 @@ class Model:
                     torch.nn.init.constant_(self.conv.bias, 0)
 
             def forward(self, x, x_deep):
-                return self.relu(self.conv(x_deep) * self.beta + x)
+                return self.relu(self.conv(x_deep) * self.beta + self.conv1(x) * self.gamma)
 
         class FlownetShallow(Module):
             def __init__(self, in_planes, c=64):
