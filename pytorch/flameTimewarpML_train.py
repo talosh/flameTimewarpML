370fccaa9be087c48289806cd9d9d6c2dfeda893
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-25 14:37:05 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2041,10 +2041,10 @@ def main():
             param.requires_grad = False
         '''
 
-        for param in flownet.module.encode.parameters():
-            param.requires_grad = False
-        for param in flownet.module.block0.parameters():
-            param.requires_grad = False     
+        # for param in flownet.module.encode.parameters():
+        #    param.requires_grad = False
+        # for param in flownet.module.block0.parameters():
+        #    param.requires_grad = False     
         # for param in flownet.module.block1.parameters():
         #    param.requires_grad = False
         for param in flownet.module.block2.parameters():
