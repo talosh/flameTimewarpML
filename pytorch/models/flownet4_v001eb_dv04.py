7e387571ca5242467a30588199f9264d88901b0a
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-27 13:27:02 +0000

    update flownet4_v001eb_dv04.py

--- pytorch/models/flownet4_v001eb_dv04.py
+++ pytorch/models/flownet4_v001eb_dv04.py
@@ -32,7 +32,7 @@ class Model:
                     stride=stride,
                     padding=padding, 
                     dilation=dilation,
-                    padding_mode='zeros',
+                    padding_mode='reflect',
                     bias=True
                 ),
                 torch.nn.LeakyReLU(0.2, True)
