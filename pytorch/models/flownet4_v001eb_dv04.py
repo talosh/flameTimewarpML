d29e0ca6955b497d3bd1bde9997bc292a22ed0d7
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-27 21:38:42 +0000

    update flownet4_v001eb_dv04.py

--- pytorch/models/flownet4_v001eb_dv04.py
+++ pytorch/models/flownet4_v001eb_dv04.py
@@ -259,6 +259,7 @@ class Model:
                 self.lastconv = torch.nn.Sequential(
                     torch.nn.ConvTranspose2d(c, c, 6, 2, 2),
                     torch.nn.Conv2d(c, c, kernel_size=3, stride=1, padding=1, padding_mode = 'reflect', bias=True),
+                    torch.nn.Mish(True),
                     torch.nn.ConvTranspose2d(c, c, 4, 2, 1),
                     torch.nn.Conv2d(c, 6, kernel_size=3, stride=1, padding=1, padding_mode = 'reflect', bias=True),
                 )
