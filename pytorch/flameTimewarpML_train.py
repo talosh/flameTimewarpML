69ca2ffd0734114264f24e20e1b79f047ac65973
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-27 15:41:46 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2289,7 +2289,7 @@ def main():
         if random.uniform(0, 1) < (args.generalize / 100):
             # add noise
             if random.uniform(0, 1) < 0.99:
-                delta = random.uniform(0, 8e-2)
+                delta = random.uniform(0, 4e-2)
                 img0 += torch.rand_like(img0) * delta
                 img2 += torch.rand_like(img1) * delta
 
