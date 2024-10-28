70e4785f7bcc9f39c01c7d9a0b2277cb83feb279
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-28 11:04:54 +0000

    update flownet4_v001eb_dv04.py

--- pytorch/models/flownet4_v001eb_dv04.py
+++ pytorch/models/flownet4_v001eb_dv04.py
@@ -428,11 +428,11 @@ class Model:
                 # step training
 
                 # stage 1
-                scale[0] = 1
+                # scale[0] = 1
 
                 # stage 2
-                # scale[0] = scale[3]
-                # scale[1] = 1
+                scale[0] = scale[3]
+                scale[1] = 1
 
                 # stage 3
                 # scale[0] = scale[2]
@@ -455,7 +455,7 @@ class Model:
                 conf_list[0] = torch.sigmoid(conf)
                 merged[0] = warp(img0, flow[:, :2]) * mask_list[0] + warp(img1, flow[:, 2:4]) * (1 - mask_list[0])
 
-                # '''
+                '''
                 # step training stage 1
                 flow_list[4] = flow_list[0]
                 mask_list[4] = mask_list[0]
@@ -463,7 +463,7 @@ class Model:
                 merged[4] = merged[0]
 
                 return flow_list, mask_list, conf_list, merged
-                # '''
+                '''
 
                 # refine step 1
                 flow_d, mask, conf_d = self.block1(
@@ -485,7 +485,7 @@ class Model:
                 conf_list[1] = torch.sigmoid(conf)
                 merged[1] = warp(img0, flow[:, :2]) * mask_list[1] + warp(img1, flow[:, 2:4]) * (1 - mask_list[1])
 
-                '''
+                # '''
                 # step training stage 2
                 flow_list[4] = flow_list[1]
                 mask_list[4] = mask_list[1]
@@ -493,7 +493,7 @@ class Model:
                 merged[4] = merged[1]
 
                 return flow_list, mask_list, conf_list, merged
-                '''
+                # '''
 
                 # refine step 2
                 flow_d, mask, conf_d = self.block2(
