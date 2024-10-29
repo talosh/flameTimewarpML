0a4cc8e561a794054bd7adbbc9897a2ae2a9d2a9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-29 16:03:16 +0000

    update flownet4_v001eb_dv04.py

--- pytorch/models/flownet4_v001eb_dv04.py
+++ pytorch/models/flownet4_v001eb_dv04.py
@@ -254,10 +254,10 @@ class Model:
         class FlownetDeepSingleHead(Module):
             def __init__(self, in_planes, c=64):
                 super().__init__()
-                self.conv0 = conv(in_planes, c//2, 3, 2, 1)
-                self.conv1 = conv(c//2, c, 3, 2, 1)
+                self.conv0 = conv(in_planes, c, 3, 2, 1)
+                self.conv1 = conv(c, c, 3, 2, 1)
                 self.conv2 = conv(c, c*2, 3, 2, 1)
-                self.attn = CBAM(c//2)
+                self.attn = CBAM(c)
                 self.conv_deep = torch.nn.Conv2d(c, c, 3, 1, 1, padding_mode = 'reflect', bias=True)
                 self.beta_deep = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
                 self.relu = torch.nn.LeakyReLU(0.2, True)
