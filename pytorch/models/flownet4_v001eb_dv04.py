69eace0ff309004aadc4fea3f9076798a1dfaf80
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-10-29 10:40:26 +0000

    update flownet4_v001eb_dv04.py

--- pytorch/models/flownet4_v001eb_dv04.py
+++ pytorch/models/flownet4_v001eb_dv04.py
@@ -265,6 +265,7 @@ class Model:
                 self.conv1 = conv_mish(c//2, c, 3, 2, 1)
                 self.conv2 = conv_mish(c, c*2, 3, 2, 1)
                 self.attn = CBAM(c//2)
+                self.reshuffle = torch.nn.Conv2d(c, c, 1, 1, 0)
                 self.convblock = torch.nn.Sequential(
                     ResConvMish(c),
                     ResConvMish(c),
@@ -324,6 +325,7 @@ class Model:
                 feat_deep = self.convblock_deep(feat_deep)
 
                 feat = torch.cat((feat, feat_deep), 1)
+                feat = self.reshuffle(feat)
                 feat = self.convblock_mix(feat)
                 tmp = self.lastconv(feat)
 
