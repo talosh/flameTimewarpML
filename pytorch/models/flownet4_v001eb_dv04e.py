8542dee3dce4ebb2c4a1c78aca1478336e061566
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-03 12:40:48 +0000

    update flownet4_v001eb_dv04e.py

--- pytorch/models/flownet4_v001eb_dv04e.py
+++ pytorch/models/flownet4_v001eb_dv04e.py
@@ -521,13 +521,13 @@ class Model:
                 # scale[0] = 1
 
                 # stage 2
-                # scale[0] = scale[3]
-                # scale[1] = 1
+                scale[0] = scale[3]
+                scale[1] = 1
 
                 # stage 3
-                scale[0] = scale[2]
-                scale[1] = scale[3]
-                scale[2] = 1
+                # scale[0] = scale[2]
+                # scale[1] = scale[3]
+                # scale[2] = 1
 
                 flow, mask, conf = self.block0(
                     img0, 
