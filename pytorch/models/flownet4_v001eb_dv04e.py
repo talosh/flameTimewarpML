f539945761b4739e8db4c5c2ed24a3c87afd15ad
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-03 14:23:03 +0000

    update flownet4_v001eb_dv04e.py

--- pytorch/models/flownet4_v001eb_dv04e.py
+++ pytorch/models/flownet4_v001eb_dv04e.py
@@ -261,7 +261,7 @@ class Model:
                 super().__init__()
                 cd = int(1.618 * c)
                 self.conv0 = conv(in_planes, c//2, 7, 1, 3)
-                self.conv0a = conv(c//2 + 2, c, 7, 2, 3)
+                self.conv0a = conv(c//2 + 2, c, 3, 2, 1)
                 self.conv1 = conv(c, c, 3, 2, 1)
                 self.conv2 = conv(c, cd, 3, 2, 1)
                 self.attn = CBAM(c//2)
