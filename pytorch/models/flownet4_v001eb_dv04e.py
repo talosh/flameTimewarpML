7492aa4750237439f6c187183314153437dd1a78
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-03 20:55:35 +0000

    update flownet4_v001eb_dv04e.py

--- pytorch/models/flownet4_v001eb_dv04e.py
+++ pytorch/models/flownet4_v001eb_dv04e.py
@@ -437,11 +437,11 @@ class Model:
                 # step training
 
                 # stage 1
-                scale[0] = 1
+                # scale[0] = 1
 
                 # stage 2
-                # scale[0] = scale[3]
-                # scale[1] = 1
+                scale[0] = scale[3]
+                scale[1] = 1
 
                 # stage 3
                 # scale[0] = scale[2]
