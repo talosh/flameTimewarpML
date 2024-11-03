1a0f19bdd1ca3dea8943163858da1e99f914e8af
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-03 07:11:04 +0000

    update flameTimewarpML_train.py and flownet4_v001eb_dv04e.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2053,18 +2053,18 @@ def main():
             param.requires_grad = False
         '''
 
-        # for param in flownet.module.encode.parameters():
+        for param in flownet.module.encode.parameters():
+            param.requires_grad = False
+        for param in flownet.module.block0.parameters():
+            param.requires_grad = False     
+        #for param in flownet.module.block1.parameters():
         #    param.requires_grad = False
-        #for param in flownet.module.block0.parameters():
-        #    param.requires_grad = False     
-        for param in flownet.module.block1.parameters():
-            param.requires_grad = False
-        for param in flownet.module.block2.parameters():
-            param.requires_grad = False
-        for param in flownet.module.block3.parameters():
-            param.requires_grad = False
-        for param in flownet.module.block4.parameters():
-            param.requires_grad = False
+        #for param in flownet.module.block2.parameters():
+        #    param.requires_grad = False
+        #for param in flownet.module.block3.parameters():
+        #    param.requires_grad = False
+        #for param in flownet.module.block4.parameters():
+        #    param.requires_grad = False
         
         '''
         for param in flownet.module.block0.lastconv_long.parameters():
