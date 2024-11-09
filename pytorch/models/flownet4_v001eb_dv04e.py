a213f10bb6edca2c7433f276088fc89e67f1e9d7
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-09 01:57:14 +0000

    update flownet4_v001eb_dv04e.py

--- pytorch/models/flownet4_v001eb_dv04e.py
+++ pytorch/models/flownet4_v001eb_dv04e.py
@@ -575,7 +575,7 @@ class Model:
                 # scale[0] = 1
 
                 # stage 2
-                scale[0] = random.randint(1, 4) # scale[1]
+                scale[0] = random.randint(1, 5) # scale[1]
                 scale[1] = 1
 
                 # stage 3
