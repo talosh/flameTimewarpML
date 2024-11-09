4f682effdd24847bad517bcd39b22370b99fe470
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-09 01:52:59 +0000

    update flownet4_v001eb_dv04e.py

--- pytorch/models/flownet4_v001eb_dv04e.py
+++ pytorch/models/flownet4_v001eb_dv04e.py
@@ -481,7 +481,7 @@ class Model:
                     # x = torch.cat((x, flow), 1)
 
                 # '''
-                spvalue = self.maxdepth * 3
+                spvalue = self.maxdepth * 4
                 _, _, sh, sw = x.shape
                 sph = ((sh - 1) // spvalue + 1) * spvalue
                 spw = ((sw - 1) // spvalue + 1) * spvalue
