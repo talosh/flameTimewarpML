bf5887c82548e73e91a29110ad8784419684678e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-11 22:51:37 +0000

    update flownet4_v001eb_dv04ea.py

--- pytorch/models/flownet4_v001eb_dv04ea.py
+++ pytorch/models/flownet4_v001eb_dv04ea.py
@@ -701,7 +701,7 @@ class Model:
                 super().__init__()
                 self.block0 = FlownetDeepSingleHead(23, c=64)
                 self.block1 = Flownet(28, c=64)
-                self.block2 = Flownet_d2(28, c=48)
+                self.block2 = Flownet_d2(28, c=64)
                 self.block3 = Flownet_d1(28, c=48)
                 self.block4 = Flownet_d1(28, c=32)
                 self.encode = Head()
