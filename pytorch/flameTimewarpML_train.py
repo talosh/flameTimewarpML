816ce4bda6b9c80d71066ba81ae08011442be53f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-13 10:25:40 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1601,7 +1601,7 @@ def diffmatte(tensor1, tensor2):
 def variance_loss(tensor, threshold):
     variance = torch.std(tensor)
     # Loss is positive when variance is less than the threshold
-    return torch.nn.functional.mish(threshold - variance) # / (threshold + 1e-8)
+    return torch.relu(threshold - variance) / (threshold + 1e-8)
 
 class LossStats:
     def __init__(self):
