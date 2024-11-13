4700318eb05ca77e7125c9022ab90e9615651260
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-13 09:37:08 +0000

    update flameTimewarpML_train.py and flownet4_v001eb_dv04ea.py

--- pytorch/models/flownet4_v001eb_dv04ea.py
+++ pytorch/models/flownet4_v001eb_dv04ea.py
@@ -598,9 +598,9 @@ class Model:
                 self.revmix2 = ResConvRevMix(c, cd)
                 self.lastconv_mask = torch.nn.Sequential(
                     torch.nn.Upsample(scale_factor=2, mode='bilinear'),
-                    conv(c//2, c//4, 3, 1, 1),
+                    conv(c//2, c//2, 3, 1, 1),
                     torch.nn.Upsample(scale_factor=2, mode='bilinear'),
-                    torch.nn.Conv2d(c//4, 2, kernel_size=3, stride=1, padding=1, padding_mode = 'zeros', bias=True)
+                    torch.nn.Conv2d(c//2, 2, kernel_size=3, stride=1, padding=1, padding_mode = 'zeros', bias=True)
                 )
                 self.lastconv_fw = torch.nn.Sequential(
                     torch.nn.Upsample(scale_factor=2, mode='bilinear'),
