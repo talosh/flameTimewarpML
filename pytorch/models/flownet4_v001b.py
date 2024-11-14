1c2327382eb686b76b4fc68e761b41a7ff34b0bb
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-14 14:16:51 +0000

    update flownet4_v001b.py

--- pytorch/models/flownet4_v001b.py
+++ pytorch/models/flownet4_v001b.py
@@ -125,7 +125,7 @@ class Model:
                 ph = self.maxdepth - (sh % self.maxdepth)
                 pw = self.maxdepth - (sw % self.maxdepth)
                 padding = (0, pw, 0, ph)
-                x = torch.nn.functional.pad(x, padding, mode='constant')
+                x = torch.nn.functional.pad(x, padding, mode='reflect')
 
                 feat = self.conv0(x)
                 feat = self.convblock(feat)
