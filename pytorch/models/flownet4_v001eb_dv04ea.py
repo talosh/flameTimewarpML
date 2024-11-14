ead88055f2eb4136672cd9536fb31971f38bd660
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-14 12:40:49 +0000

    update flownet4_v001eb_dv04ea.py

--- pytorch/models/flownet4_v001eb_dv04ea.py
+++ pytorch/models/flownet4_v001eb_dv04ea.py
@@ -668,7 +668,7 @@ class Model:
                 self.block0 = Flownet(2*3 + 2*8, c=192)
                 self.block1 = Flownet(2*3 + 2*8 + 4 + 1, c=128)
                 self.block2 = Flownet(2*3 + 2*8 + 4 + 1, c=96)
-                self.block3 = Flownet(2*3 + 2*8 + 4 + 1, c=64)
+                self.block3 = Flownet_d1(2*3 + 4 + 2, c=24)
                 self.block4 = Flownet_d1(2*3 + 4 + 2, c=24)
                 self.encode = Head()
 
