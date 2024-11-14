6beeadc67259be3556dbc6082a2b67b550815ed5
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-14 16:37:58 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1602,7 +1602,8 @@ def variance_loss(tensor, threshold):
     mean = tensor.mean()
     variance = tensor.std()
     mean_deviation = abs(mean - 0.5)
-    variance_loss = torch.relu(threshold - variance).item() # / (threshold + 1e-11)
+    variance_loss = threshold - variance if threshold - variance > 0 else 0
+    # variance_loss = torch.relu(threshold - variance).item() # / (threshold + 1e-11)
     return variance_loss + 0.1 * mean_deviation
 
 class LossStats:
