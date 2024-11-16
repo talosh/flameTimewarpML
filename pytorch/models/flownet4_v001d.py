815837634b157bfbb6212c1c8de5c232432901a4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-16 21:50:37 +0000

    update flownet4_v001d.py

--- pytorch/models/flownet4_v001d.py
+++ pytorch/models/flownet4_v001d.py
@@ -213,6 +213,7 @@ class Model:
 
                 fs2 = ((flow.shape[2] - 1.0) / 2.0)
                 fs3 = ((flow.shape[3] - 1.0) / 2.0)
+                flow[:, 0:1, :, :] = flow[:, 0:1, :, :] * fs3
 
                 '''
                 flow_list[3] = flow # .detach().clone() # torch.tanh(flow.detach().clone())
