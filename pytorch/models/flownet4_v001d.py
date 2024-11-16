a6ee5b54276f26de9044db9500052ab0f97e214a
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-16 21:59:11 +0000

    update flownet4_v001d.py

--- pytorch/models/flownet4_v001d.py
+++ pytorch/models/flownet4_v001d.py
@@ -34,7 +34,7 @@ class Model:
                 # torch.nn.SELU(inplace = True)
             )
 
-        def warp(tenInput, tenFlow):
+        def warp_old(tenInput, tenFlow):
             k = (str(tenFlow.device), str(tenFlow.size()))
             if k not in backwarp_tenGrid:
                 tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
@@ -45,7 +45,7 @@ class Model:
             g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
             return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
 
-        def warp_norm(tenInput, tenFlow):
+        def warp(tenInput, tenFlow):
             k = (str(tenFlow.device), str(tenFlow.size()))
             if k not in backwarp_tenGrid_norm:
                 tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3]).view(1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
