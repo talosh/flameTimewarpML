85e012046cd08d5d2d3509d30ea177d36105da10
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-16 22:46:44 +0000

    update flownet4_v001d.py

--- pytorch/models/flownet4_v001d.py
+++ pytorch/models/flownet4_v001d.py
@@ -93,7 +93,7 @@ class Model:
                     torch.nn.ConvTranspose2d(c, 4*6, 4, 2, 1),
                     torch.nn.PixelShuffle(2)
                 )
-                self.maxdepth = 4
+                self.maxdepth = 8
 
             def forward(self, img0, img1, f0, f1, timestep, mask, flow, scale=1):
                 n, c, h, w = img0.shape
