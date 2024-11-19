1f4049b589a0500871319b1d2898d2bd588dcc9d
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-19 20:42:13 +0000

    update flownet4_v001hp5.py

--- pytorch/models/flownet4_v001hp5.py
+++ pytorch/models/flownet4_v001hp5.py
@@ -41,6 +41,7 @@ class Model:
             # tenFlow = torch.cat([ tenFlow[:, 0:1, :, :] / ((tenInput.shape[3] - 1.0) / 2.0), tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0) ], 1)
 
             g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
+            print (g.dtype)
             return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
 
         class ChannelAttention(Module):
