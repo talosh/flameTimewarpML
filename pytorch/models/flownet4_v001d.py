af0d9699393c3f830180da12626f00e3b16d60ca
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-11-20 23:36:29 +0000

    update flownet4_v001d.py

--- pytorch/models/flownet4_v001d.py
+++ pytorch/models/flownet4_v001d.py
@@ -335,7 +335,7 @@ class Model:
         class FlownetCas(Module):
             def __init__(self):
                 super().__init__()
-                self.block0 = FlownetDeepSingleHead(6+16+1+2, c=192)
+                self.block0 = FlownetDeepSingleHead(6+16+1+2+2, c=192)
                 self.block1 = Flownet(8+4+16, c=128)
                 self.block2 = Flownet(8+4+16, c=96)
                 self.block3 = Flownet(8+4+16, c=64)
