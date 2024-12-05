c49bd25e474d50cb52c1e3df12f31fba7dd0ae99
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-12-05 12:46:21 +0000

    update flownet4_v001f.py

--- pytorch/models/flownet4_v001f.py
+++ pytorch/models/flownet4_v001f.py
@@ -570,7 +570,7 @@ class Model:
                     'conf_list': conf_list,
                     'merged': merged,
                     'merged_distill': merged_dist,
-                    'loss_distill': None
+                    'loss_distill': 0
                 }
 
                 return result
