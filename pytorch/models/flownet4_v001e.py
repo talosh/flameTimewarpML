53325173b5922003a9c7c9ad7efaa76359ee8997
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-12-06 12:29:15 +0000

    update flownet4_v001e.py

--- pytorch/models/flownet4_v001e.py
+++ pytorch/models/flownet4_v001e.py
@@ -486,8 +486,8 @@ class Model:
                 # img1 = img1.to(dtype = src_dtype)
 
                 flow_list[3] = flow
-                conf_list[3] = compress(conf) # torch.sigmoid(conf)
-                mask_list[3] = compress(mask) # torch.sigmoid(mask)
+                conf_list[3] = torch.sigmoid(conf) # compress(conf) # 
+                mask_list[3] = torch.sigmoid(mask) # compress(mask) # 
                 merged[3] = warp(img0, flow[:, :2]) * mask_list[3] + warp(img1, flow[:, 2:4]) * (1 - mask_list[3])
 
                 result = {
