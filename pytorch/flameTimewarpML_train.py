611ecff8db408f079b591c17e0ac41baecaede47
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-02 15:27:04 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1953,7 +1953,7 @@ def main():
     parser.add_argument('--lr', type=float, default=1e-6, help='Learning rate (default: 1e-6)')
     parser.add_argument('--type', type=int, default=1, help='Model type (int): 1 - MultiresNet, 2 - MultiresNet 4 (default: 1)')
     parser.add_argument('--pulse', type=float, default=10000, help='Period in steps to pulse learning rate (float) (default: 10K)')
-    parser.add_argument('--pulse_amplitude', type=float, default=25, help='Learning rate pulse amplitude (percentage) (default: 25)')
+    parser.add_argument('--pulse_amplitude', type=float, default=1000, help='Learning rate pulse amplitude (percentage) (default: 1000)')
     parser.add_argument('--onecycle', type=int, default=-1, help='Train one cycle for N epochs (default: None)')
     parser.add_argument('--cyclic', type=int, default=-1, help='Use cyclic LR scheduler')
     parser.add_argument('--state_file', type=str, default=None, help='Path to the pre-trained model state dict file (optional)')
