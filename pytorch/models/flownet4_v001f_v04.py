bd36e2bcc72b473bee7c788dd1203d2d6ffbef84
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-03 17:44:27 +0000

    update flownet4_v001f_v04.py

--- pytorch/models/flownet4_v001f_v04.py
+++ pytorch/models/flownet4_v001f_v04.py
@@ -471,8 +471,8 @@ class Model:
                 super().__init__()
                 self.block0 = FlownetDeepDualHead(6+20+1+2, 6+20+1+2+4, c=192) # images + feat + timestep + lingrid
                 self.block1 = FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=144)  # images + feat + timestep + lingrid + mask + conf + flow
-                self.block2 = FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=128) # images + feat + timestep + lingrid + mask + conf + flow
-                self.block3 = FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=112) # images + feat + timestep + lingrid + mask + conf + flow
+                self.block2 = None # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=128) # images + feat + timestep + lingrid + mask + conf + flow
+                self.block3 = None # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=112) # images + feat + timestep + lingrid + mask + conf + flow
                 self.encode = Head()
                 self.encode_xf = HeadF()
 
