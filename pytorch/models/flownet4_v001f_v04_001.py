51772895b67ea7185108878478fd49cfc171e994
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-07 15:17:01 +0000

    update flownet4_v001f_v04_001.py

--- pytorch/models/flownet4_v001f_v04_001.py
+++ pytorch/models/flownet4_v001f_v04_001.py
@@ -310,7 +310,7 @@ class Model:
                 
                 warped_img0 = warp(img0, flow[:, :2])
                 warped_img1 = warp(img1, flow[:, 2:4])
-                x = torch.cat((warped_img0, warped_img1, timestep, mask), 1)
+                x = torch.cat((warped_img0, warped_img1, timestep, mask, conf), 1)
                 x = torch.nn.functional.interpolate(x, size=(sh, sw), mode="bilinear", align_corners=False)
                 flow = torch.nn.functional.interpolate(flow, size=(sh, sw), mode="bilinear", align_corners=False) * 1. / scale
                 x = torch.cat((x, flow), 1)
