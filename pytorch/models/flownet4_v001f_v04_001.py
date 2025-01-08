c148081817b2da78cbde6328676069439ff3fa37
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-08 10:26:05 +0000

    update flownet4_v001f_v04_001.py

--- pytorch/models/flownet4_v001f_v04_001.py
+++ pytorch/models/flownet4_v001f_v04_001.py
@@ -297,7 +297,7 @@ class Model:
                 )
                 self.maxdepth = 4
 
-            def forward(self, img0, img1, timestep, mask, conf, flow, scale=1):
+            def forward(self, img0, img1, timestep, mask, conf, scale=1):
                 n, c, h, w = img0.shape
                 sh, sw = round(h * (1 / scale)), round(w * (1 / scale))
                 ph = self.maxdepth - (sh % self.maxdepth)
@@ -313,14 +313,14 @@ class Model:
                 warped_img1 = warp(img1, flow[:, 2:4])
                 x = torch.cat((warped_img0, warped_img1, timestep, mask, conf), 1)
                 x = torch.nn.functional.interpolate(x, size=(sh, sw), mode="bilinear", align_corners=False)
-                flow = torch.nn.functional.interpolate(flow, size=(sh, sw), mode="bilinear", align_corners=False) * 1. / scale
-                x = torch.cat((x, flow), 1)
+                # flow = torch.nn.functional.interpolate(flow, size=(sh, sw), mode="bilinear", align_corners=False) * 1. / scale
+                # x = torch.cat((x, flow), 1)
 
                 x = torch.nn.functional.pad(x, padding, mode='constant')
 
                 feat = self.conv0(x)
                 feat = self.convblock(feat)
-                tmp = self.lastconv(feat)
+                tmp = self.lastconv(feat)[:, :, :sh, :sw]
                 tmp = torch.nn.functional.interpolate(tmp[:, :, :sh, :sw], size=(h, w), mode="bilinear", align_corners=False)
                 flow = tmp[:, :4] * scale
                 mask = tmp[:, 4:5]
@@ -488,7 +488,7 @@ class Model:
                 self.block0 = FlownetDeepDualHead(6+20+1+2, 6+20+1+2+4, c=192) # images + feat + timestep + lingrid
                 self.block1 = FlownetDeepDualHead(6+20+1+1+2+1+4, 12+20+1, c=96) # Flownet(6+20+1+1+1+4, c=144)  # images + feat + timestep + lingrid + mask + conf + flow
                 self.block2 = FlownetDeepDualHead(6+20+1+1+2+1+4, 12+20+1, c=64) # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=128) # images + feat + timestep + lingrid + mask + conf + flow
-                self.block3 = FlownetLT(6+2+1+1+1+4, c=48) # None # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=112) # images + feat + timestep + lingrid + mask + conf + flow
+                self.block3 = FlownetLT(6+2+1+1+1, c=48) # None # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=112) # images + feat + timestep + lingrid + mask + conf + flow
                 self.encode = Head()
                 self.encode_xf = HeadF()
 
