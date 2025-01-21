6b7f6e850555051c3ce2c4415e53053a95fc7b96
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-21 16:14:03 +0000

    update flownet4_v001f_v04_001.py

--- pytorch/models/flownet4_v001f_v04_001.py
+++ pytorch/models/flownet4_v001f_v04_001.py
@@ -499,7 +499,7 @@ class Model:
             def __init__(self):
                 super().__init__()
                 self.block0 = FlownetDeepDualHead(6+20+1+2, 6+20+1+2+4, c=192) # images + feat + timestep + lingrid
-                self.block1 = FlownetDeepDualHead(6+20+1+1+2+1+4, 12+20+1, c=144) # Flownet(6+20+1+1+1+4, c=144)  # images + feat + timestep + lingrid + mask + conf + flow
+                # self.block1 = FlownetDeepDualHead(6+20+1+1+2+1+4, 12+20+1, c=144) # Flownet(6+20+1+1+1+4, c=144)  # images + feat + timestep + lingrid + mask + conf + flow
                 self.block2 = FlownetDeepDualHead(6+20+1+1+2+1+4, 12+20+1, c=128) # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=128) # images + feat + timestep + lingrid + mask + conf + flow
                 self.block3 = FlownetDeepDualHead(6+20+1+1+2+1+4, 12+20+1, c=96) # FlownetLT(6+2+1+1+1, c=48) # None # FlownetDeepDualHead(9+30+1+1+4+1+2, 22+30+1, c=112) # images + feat + timestep + lingrid + mask + conf + flow
                 self.blockf = FlownetLT(11, c=48)
