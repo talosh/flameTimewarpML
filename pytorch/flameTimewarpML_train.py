513baf3c01748eadd420d1239a6805e470c628a3
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-22 23:44:29 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2759,7 +2759,7 @@ def main():
         loss_lap = criterion_lap(output_clean, img1_orig)
         loss_conf = criterion_l1(output_clean, diffmatte(output_clean, img1_orig))
         # loss_l1 = criterion_l1(output_clean, img1_orig)
-        loss = loss + loss_l1 + loss_lap +  + 1e-2*loss_conf + 1e-2 * float(torch.mean(loss_LPIPS).item())
+        loss = loss + loss_l1 + loss_lap + 1e-2*loss_conf + 1e-2 * float(torch.mean(loss_LPIPS).item())
 
         min_l1 = min(min_l1, float(loss_l1.item()))
         max_l1 = max(max_l1, float(loss_l1.item()))
