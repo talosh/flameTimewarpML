2d6d1a5ded9895a78a201c866237b3ad78781f82
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-22 23:42:06 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2723,6 +2723,7 @@ def main():
                 output_clean = warp(img0_orig, flow0) * mask + warp(img2_orig, flow1) * (1 - mask)
                 loss_l1 = criterion_l1(output_clean, img1_orig)
                 loss_mask = variance_loss(mask, 0.1)
+                loss = loss + loss_l1 + 1e-2*loss_mask
 
                 # output_scaled = torch.nn.functional.interpolate(output, scale_factor= 1. / scale, mode='bilinear')
                 # img1_scaled = torch.nn.functional.interpolate(img1_orig, scale_factor= 1. / scale, mode='bilinear')
@@ -2758,7 +2759,7 @@ def main():
         loss_lap = criterion_lap(output_clean, img1_orig)
         loss_conf = criterion_l1(output_clean, diffmatte(output_clean, img1_orig))
         # loss_l1 = criterion_l1(output_clean, img1_orig)
-        loss = loss + loss_l1 + loss_lap + 1e-2 * float(torch.mean(loss_LPIPS).item())
+        loss = loss + loss_l1 + loss_lap +  + 1e-2*loss_conf + 1e-2 * float(torch.mean(loss_LPIPS).item())
 
         min_l1 = min(min_l1, float(loss_l1.item()))
         max_l1 = max(max_l1, float(loss_l1.item()))
