e974bf2c7778318ff83d9ca61f9ce5b72cd33398
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-22 23:46:19 +0000

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -2723,7 +2723,8 @@ def main():
                 output_clean = warp(img0_orig, flow0) * mask + warp(img2_orig, flow1) * (1 - mask)
                 loss_l1 = criterion_l1(output_clean, img1_orig)
                 loss_mask = variance_loss(mask, 0.1)
-                loss = loss + loss_l1 + 1e-2*loss_mask
+                loss_conf = criterion_l1(conf, diffmatte(output_clean, img1_orig))
+                loss = loss + loss_l1 + 1e-2*loss_mask + 1e-2*loss_conf
 
                 # output_scaled = torch.nn.functional.interpolate(output, scale_factor= 1. / scale, mode='bilinear')
                 # img1_scaled = torch.nn.functional.interpolate(img1_orig, scale_factor= 1. / scale, mode='bilinear')
@@ -2757,9 +2758,8 @@ def main():
         diff_matte = diffmatte(output_clean, img1_orig)
         loss_LPIPS = loss_fn_alex(output_clean * 2 - 1, img1_orig * 2 - 1)
         loss_lap = criterion_lap(output_clean, img1_orig)
-        loss_conf = criterion_l1(conf, diffmatte(output_clean, img1_orig))
         # loss_l1 = criterion_l1(output_clean, img1_orig)
-        loss = loss + loss_l1 + loss_lap +  + 1e-2*loss_conf + 1e-2 * float(torch.mean(loss_LPIPS).item())
+        loss = loss + loss_l1 + loss_lap + 1e-2 * float(torch.mean(loss_LPIPS).item())
 
         min_l1 = min(min_l1, float(loss_l1.item()))
         max_l1 = max(max_l1, float(loss_l1.item()))
