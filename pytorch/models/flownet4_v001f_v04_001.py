18adb6d00242f48fc293e9b11585e0d427119f4c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-27 10:57:01 +0000

    update flownet4_v001f_v04_001.py

--- pytorch/models/flownet4_v001f_v04_001.py
+++ pytorch/models/flownet4_v001f_v04_001.py
@@ -442,7 +442,7 @@ class Model:
                     # add noise on fisrt iteration:
                     if iteration == 0:
                         for i in range(10):
-                            flow = flow + torch.rand_like(flow) * scale
+                            flow = flow + (torch.rand_like(flow) * 2 - 1) * scale
 
                     # delta = torch.abs(torch.max(flow) - torch.min(flow))
                     # flow_noise = torch.rand_like(flow) * delta
