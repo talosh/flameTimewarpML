e1050e68bf76377607dd3b373c35d93e0c517749
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-01-31 09:53:14 +0000

    update flownet4_v001f_v04_001.py, flownet4_v001f_v04_002.py and flownet4_v001f_v04_003.py

--- pytorch/models/flownet4_v001f_v04_002.py
+++ pytorch/models/flownet4_v001f_v04_002.py
@@ -345,9 +345,9 @@ class Model:
                 super().__init__()
                 cd = 1 * round(1.618 * c) + 2 - (1 * round(1.618 * c) % 2)
                 self.conv0 = conv(in_planes, c//2, 3, 2, 1)
-                self.conv0f = conv(in_planes_fx, c//2, 3, 2, 1)
+                self.conv0f = conv(in_planes_fx, c, 3, 2, 1)
                 self.conv1 = conv(c//2, c, 3, 2, 1)
-                self.conv1f = conv(c//2, c, 3, 2, 1)
+                self.conv1f = conv(c, c, 3, 2, 1)
                 self.conv2f = conv(c, cd, 3, 2, 1)
                 self.convblock1 = torch.nn.Sequential(
                     ResConv(c),
