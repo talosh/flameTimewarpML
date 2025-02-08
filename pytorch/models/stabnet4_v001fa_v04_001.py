6fa186d918a6482b9b515a77ef645bb3c01e3a22
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-02-08 23:16:44 +0000

    update stabnet4_v001fa_v04_001.py

--- pytorch/models/stabnet4_v001fa_v04_001.py
+++ pytorch/models/stabnet4_v001fa_v04_001.py
@@ -343,9 +343,9 @@ class Model:
                 super().__init__()
                 cd = 1 * round(1.618 * c) + 2 - (1 * round(1.618 * c) % 2)
                 self.conv0 = conv(in_planes, c//2, 3, 2, 1)
-                self.conv0f = conv(in_planes_fx, c, 3, 2, 1)
+                self.conv0f = conv(in_planes_fx, c//2, 3, 2, 1)
                 self.conv1 = conv(c//2, c, 3, 2, 1)
-                self.conv1f = conv(c, c, 3, 2, 1)
+                self.conv1f = conv(c//2, c, 3, 2, 1)
                 self.conv2 = conv(c, cd, 3, 2, 1)
                 self.convblock1 = torch.nn.Sequential(
                     ResConv(c),
