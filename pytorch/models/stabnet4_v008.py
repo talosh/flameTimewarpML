e0d78fb7f7dc930f492143b43561aa6cffc7043c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-02-20 15:41:43 +0000

    update stabnet4_v008.py

--- pytorch/models/stabnet4_v008.py
+++ pytorch/models/stabnet4_v008.py
@@ -163,7 +163,9 @@ class Model:
             def __init__(self, c, dilation=1):
                 super().__init__()
                 self.attnblock = torch.nn.Sequential(
-                    ResConv(c)
+                    torch.nn.Conv2d(c, c//4, 3, 1, 1),
+                    ResConv(c),
+                    torch.nn.Conv2d(c//4, c, 3, 1, 1),
                 )
                 self.conv = torch.nn.Conv2d(c, c, 3, 1, 1, dilation = dilation, groups = 1, padding_mode = 'zeros', bias=True)
                 self.beta = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
