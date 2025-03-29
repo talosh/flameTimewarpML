d93eee421b513a489d894356d685a150bc6e49c4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-03-29 11:26:52 +0000

    update flownet5_v003.py

--- pytorch/models/flownet5_v003.py
+++ pytorch/models/flownet5_v003.py
@@ -36,7 +36,7 @@ class Model:
                 alpha = alpha.view(1, -1, *([1] * (x.dim() - 2)))
                 
                 # Apply PR_ELU activation function
-                return torch.where(x > 0, x, alpha * (torch.exp(x) - 1))
+                return torch.where(x > 0, x, alpha * torch.nn.ELU(x))
 
         def conv(in_planes, out_planes, kernel_size=3, stride=1, padding=1, dilation=1):
             return torch.nn.Sequential(
