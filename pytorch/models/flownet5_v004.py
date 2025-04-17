147ba1a5513a966c612ac73019a1323bb9b09f68
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-04-17 05:43:02 +0100

    update flownet5_v004.py

--- pytorch/models/flownet5_v004.py
+++ pytorch/models/flownet5_v004.py
@@ -517,12 +517,22 @@ class Model:
                 conf_list = [None] * 4
                 merged = [None] * 4
 
+                scale[0] = 1
                 flow, mask, conf = self.block0(img0, img1, f0, f1, timestep, None, None, None, scale=scale[0])
 
                 flow_list[0] = flow.clone()
                 conf_list[0] = torch.sigmoid(conf.clone())
                 mask_list[0] = torch.sigmoid(mask.clone())
                 merged[0] = warp(img0, flow[:, :2]) * mask_list[0] + warp(img1, flow[:, 2:4]) * (1 - mask_list[0])
+
+                result = {
+                    'flow_list': flow_list,
+                    'mask_list': mask_list,
+                    'conf_list': conf_list,
+                    'merged': merged
+                }
+
+                return result
 
                 flow, mask, conf = self.block1(
                     img0, 
