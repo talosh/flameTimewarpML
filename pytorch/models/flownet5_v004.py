e753ec4e2e10dd2210dbfdcd48b42ea0398c95f6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-04-19 03:28:50 +0100

    update flownet5_v004.py and flownet5_v005.py

--- pytorch/models/flownet5_v004.py
+++ pytorch/models/flownet5_v004.py
@@ -593,7 +593,7 @@ class Model:
                 self.block3 = Flownet(31, c=64)
                 self.encode = Head()
 
-            def forward(self, img0, img1, timestep=0.5, scale=[21, 13, 8, 5, 3, 1], iterations=4, gt=None):
+            def forward(self, img0, img1, timestep=0.5, scale=[24, 16, 12, 8, 4, 1], iterations=4, gt=None):
 
                 img0 = ACEScg2cct(compress(img0))
                 img1 = ACEScg2cct(compress(img1))
@@ -665,7 +665,7 @@ class Model:
 
                 return result
 
-        self.model = FlownetCas
+        self.model = FlownetCasEval
         self.training_model = FlownetCas
 
     @staticmethod
