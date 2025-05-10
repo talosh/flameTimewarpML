df6b2ed13e19aca10ee0bbeeb04c9dbfbf48bce6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-05-10 17:32:57 +0100

    update flameTimewarpML_findscale.py

--- pytorch/flameTimewarpML_findscale.py
+++ pytorch/flameTimewarpML_findscale.py
@@ -1917,7 +1917,8 @@ def main():
 
     lr = args.lr
     # optimizer_net = torch.optim.AdamW([scale_tensor], lr=lr, betas=(0.4, 0.999))
-    optimizer_net = torch.optim.SGD([scale_tensor], lr=lr)
+    # optimizer_net = torch.optim.SGD([scale_tensor], lr=lr)
+    optimizer_net = torch.optim.Adam([scale_tensor], lr=lr)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_net, 'min', factor=0.1, patience=args.patience)
     epoch = 0
     optimizer_net.zero_grad()
