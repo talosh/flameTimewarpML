34440a91f0696c7aabd4323399e6a62253e11842
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-05-10 18:23:27 +0100

    update flameTimewarpML_findscale.py

--- pytorch/flameTimewarpML_findscale.py
+++ pytorch/flameTimewarpML_findscale.py
@@ -2036,10 +2036,8 @@ def main():
                 eval_loss_LPIPS = loss_fn_alex(eval_result * 2 - 1, eval_img1 * 2 - 1)
                 eval_lpips.append(float(torch.mean(eval_loss_LPIPS).item()))
 
-                loss = eval_loss_l1 + 2e-1 * eval_loss_LPIPS + 0.0 * scale_tensor.sum()
+                loss = eval_loss_l1 + 2e-1 * eval_loss_LPIPS + 1e-10 * scale_tensor.sum()
                 loss.backward()
-                optimizer_net.step()
-
 
                 if torch.cuda.is_available():
                     torch.cuda.synchronize()
