0af9d05947a4daa43a5fb065de72b62f8d1f0828
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-05-28 13:13:02 +0100

    update flownet5_v009.py

--- pytorch/models/flownet5_v009.py
+++ pytorch/models/flownet5_v009.py
@@ -19,15 +19,15 @@ class Model:
         class myPReLU(Module):
             def __init__(self, c):
                 super().__init__()
-                # self.alpha = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
-                # self.beta = torch.nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+                self.alpha = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
+                self.beta = torch.nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
                 self.prelu = torch.nn.PReLU(c, 0.1)
                 self.tanh = torch.nn.Tanh()
                 # self.elu = torch.nn.ELU()
 
             def forward(self, x):
-                alpha = 0.4 # * self.alpha.clamp(min=1e-8)
-                beta = 0.69 # + self.beta
+                alpha = 0.4 * self.alpha.clamp(min=1e-8)
+                beta = 0.69 + self.beta
                 x = x / alpha - beta
                 tanh_x = self.tanh(x)
                 x = torch.where(
@@ -257,7 +257,7 @@ class Model:
 
                 self.beta_conv = torch.nn.Parameter(torch.ones((1, c, 1, 1)))
                 self.beta_fourier = torch.nn.Parameter(torch.ones((1, c, 1, 1)))
-                self.relu = myPReLU(c) # torch.nn.PReLU(c, 0.2)
+                self.relu = torch.nn.PReLU(c, 0.2)
                 self.mlp = FeatureModulator(1, c)
 
             def forward(self, x):
@@ -288,6 +288,7 @@ class Model:
                 self.beta_conv = torch.nn.Parameter(torch.ones((1, c, 1, 1)))
                 self.beta_fourier = torch.nn.Parameter(torch.ones((1, c, 1, 1)))
                 self.relu = torch.nn.PReLU(c, 0.2)
+                self.mlp = FeatureModulator(1, c)
 
             def forward(self, x):
                 B, C, H, W = x.shape
