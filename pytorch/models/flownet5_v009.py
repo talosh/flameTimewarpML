31a704bc0fdf136028ba5b77f3609ebba369e123
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-05-29 15:35:15 +0100

    update flownet5_v009.py

--- pytorch/models/flownet5_v009.py
+++ pytorch/models/flownet5_v009.py
@@ -19,8 +19,8 @@ class Model:
         class myPReLU(Module):
             def __init__(self, c):
                 super().__init__()
-                # self.alpha = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
-                # self.beta = torch.nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+                self.alpha = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
+                self.beta = torch.nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
                 self.prelu = torch.nn.PReLU(c, 0.2)
                 self.tanh = torch.nn.Tanh()
                 # self.elu = torch.nn.ELU()
