208090b9b9066c0d3f5ddbe527bffbe7f8d2bad8
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-06 11:07:51 +0100

    update flownet5_v010.py

--- pytorch/models/flownet5_v010.py
+++ pytorch/models/flownet5_v010.py
@@ -198,7 +198,7 @@ class Model:
         class Mix(Module):
             def __init__(self, c, cd):
                 super().__init__()
-                self.conv0 = torch.nn.ConvTranspose2d(cd, c, 8, 4, 2)
+                self.conv0 = torch.nn.ConvTranspose2d(cd, c, 4, 2, 1)
                 self.conv1 = torch.nn.Conv2d(c, c, 3, 1, 1)
                 self.beta = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
                 self.gamma = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
