ed782942c28c0cef09893ca9bb73f4f198b5f521
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-06 16:55:57 +0100

    update flameTimewarpML_train.py

--- pytorch/flameTimewarpML_train.py
+++ pytorch/flameTimewarpML_train.py
@@ -1755,6 +1755,7 @@ def main():
     parser.add_argument('--first_epoch', type=int, default=-1, help='Epoch (int) (default: Saved)')
     parser.add_argument('--epochs', type=int, default=-1, help='Number of epoch to run (int) (default: Unlimited)')
     parser.add_argument('--reset_stats', action='store_true', dest='reset_stats', default=False, help='Reset saved step, epoch and loss stats')
+    parser.add_argument('--acc', type=int, default=-4, help='Gradient accumulation steps (int) (default: 4)')
 
     parser.add_argument('--eval', type=int, dest='eval', default=-1, help='Evaluate after N steps')
     parser.add_argument('--eval_first', action='store_true', dest='eval_first', default=False, help='Reset saved step, epoch and loss stats')
@@ -2637,8 +2638,10 @@ def main():
 
         loss.backward()
         torch.nn.utils.clip_grad_norm_(flownet.parameters(), 1)
-        optimizer_flownet.step()
-        optimizer_flownet.zero_grad()
+
+        if batch_idx % args.acc == 0:
+            optimizer_flownet.step()
+            optimizer_flownet.zero_grad()
 
         if isinstance(scheduler_flownet, torch.optim.lr_scheduler.ReduceLROnPlateau):
             pass
