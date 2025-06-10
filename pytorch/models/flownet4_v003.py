f36b2cd678d28f40758ab3905e86868a548940f6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-10 14:42:25 +0100

    update flownet4_v003.py

--- pytorch/models/flownet4_v003.py
+++ pytorch/models/flownet4_v003.py
@@ -141,7 +141,7 @@ class Model:
         class ResConv(Module):
             def __init__(self, c, dilation=1):
                 super().__init__()
-                self.conv = torch.nn.Conv2d(c, c, 3, 1, dilation, dilation = dilation, groups = 1, padding_mode = 'reflect', bias=True)
+                self.conv = torch.nn.Conv2d(c, c, 5, 1, 2, padding_mode = 'reflect', bias=True)
                 self.beta = torch.nn.Parameter(torch.ones((1, c, 1, 1)), requires_grad=True)
                 self.relu = torch.nn.PReLU(c, 0.2)
                 self.mlp = FeatureModulator(1, c)
