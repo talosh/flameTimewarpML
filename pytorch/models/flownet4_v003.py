19a35297cd22be3c7c668db859961e8e7065abce
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-10 14:01:08 +0100

    update flownet4_v003.py

--- pytorch/models/flownet4_v003.py
+++ pytorch/models/flownet4_v003.py
@@ -225,9 +225,9 @@ class Model:
             def __init__(self):
                 super().__init__()
                 self.block0 = Flownet(6+16+2, c=192)
-                self.block1 = Flownet(6+16+2+4, c=128)
-                self.block2 = Flownet(6+16+2+4, c=96)
-                self.block3 = Flownet(6+16+2+4, c=64)
+                self.block1 = Flownet(6+16+2+4+2, c=128)
+                self.block2 = Flownet(6+16+2+4+2, c=96)
+                self.block3 = Flownet(6+16+2+4+2, c=64)
                 self.encode = Head()
 
             def forward(self, img0, img1, timestep=0.5, scale=[12, 8, 4, 1], iterations=1, gt=None):
