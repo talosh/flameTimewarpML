249bef0ab268dee032f433d8a9493f0a6c691c86
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-12 00:23:00 +0100

    update flownet4_v004.py

--- pytorch/models/flownet4_v004.py
+++ pytorch/models/flownet4_v004.py
@@ -104,7 +104,7 @@ class Model:
                 # self.weight_real = torch.nn.Parameter(torch.ones(1, c, 1, 1))
                 # self.weight_imag = torch.nn.Parameter(torch.ones(1, c, 1, 1))
 
-                self.alpha = torch.nn.Parameter(torch.full((1, c, 1, 1), 0.2), requires_grad=True)
+                self.alpha = torch.nn.Parameter(torch.full((1, c, 1, 1), 0.1), requires_grad=True)
 
                 self.encoder = torch.nn.Sequential(
                     # torch.nn.Conv2d(c+2, out_channels, 3, 2, 1),
