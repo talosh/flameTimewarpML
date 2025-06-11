0087202036a291ff9633ac35d487841fec7cef50
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-11 14:24:30 +0100

    update flownet4_v004.py

--- pytorch/models/flownet4_v004.py
+++ pytorch/models/flownet4_v004.py
@@ -117,6 +117,12 @@ class Model:
                 self.c = c
 
             def forward(self, x):
+                B, C, H, W = x.shape
+                x_fft = torch.fft.rfft2(x, norm='ortho')  # [B, C, H, W//2 + 1]
+                _, _, sh, sw = x_fft.shape
+                mag = x_fft.abs()
+                phase = x_fft.angle()
+
                 return x
                 x_feat = torch.cat([x_fft.real, x_fft.imag], dim=1)
                 latent = self.encoder(x_feat)
