a62f214426ae25a2178cb2dc437c206b736ed936
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-13 00:05:04 +0100

    update flownet5_v011.py

--- pytorch/models/flownet5_v011.py
+++ pytorch/models/flownet5_v011.py
@@ -866,8 +866,8 @@ class Model:
         #    param.requires_grad = True
         #for param in net.block0.convblock_last[2].mlp.parameters():
         #    param.requires_grad = True
-        # for param in net.block0.convblock_last[3].mlp.parameters():
-        #    param.requires_grad = True
+        for param in net.block0.convblock_last[3].parameters():
+            param.requires_grad = True
         #for param in net.block0.convblock_last_shallow[0].mlp.parameters():
         #    param.requires_grad = True
         #for param in net.block0.convblock_last_shallow[1].mlp.parameters():
