3f30c202d53ede97141d86559ee3a44d9deb3476
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-13 00:46:17 +0100

    update flownet5_v011.py

--- pytorch/models/flownet5_v011.py
+++ pytorch/models/flownet5_v011.py
@@ -181,9 +181,9 @@ class Model:
                 self.alpha = torch.nn.Parameter(torch.full((1, c, 1, 1), 1.0), requires_grad=True)
 
                 self.precomp = torch.nn.Sequential(
-                    torch.nn.Conv2d(c + 2, 2 * c, 3, 2, 1),
-                    torch.nn.PReLU(2 * c, 0.2),
-                    torch.nn.Conv2d(2 * c, c, 3, 2, 1),
+                    torch.nn.Conv2d(c + 2, c, 3, 2, 1),
+                    torch.nn.PReLU(c, 0.2),
+                    torch.nn.Conv2d(c, c, 3, 2, 1),
                     torch.nn.PReLU(c, 0.2),
                 )
 
