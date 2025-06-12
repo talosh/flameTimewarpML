9f546a43a850d6f3b9c1aba76f67410f93e077cd
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-12 23:42:53 +0100

    update flownet5_v011.py

--- pytorch/models/flownet5_v011.py
+++ pytorch/models/flownet5_v011.py
@@ -425,13 +425,13 @@ class Model:
                     ResConvDummy(c),
                     ResConvDummy(c),
                     ResConvDummy(c),
-                    ResConvDummy(c),
+                    ResConvEmb(c),
                 )
                 self.convblock_last_shallow = torch.nn.Sequential(
                     ResConvDummy(c//2),
                     ResConvDummy(c//2),
                     ResConvDummy(c//2),
-                    ResConvDummy(c//2),
+                    ResConvEmb(c//2),
                 )
                 self.convblock_deep1 = torch.nn.Sequential(
                     ResConv(cd),
