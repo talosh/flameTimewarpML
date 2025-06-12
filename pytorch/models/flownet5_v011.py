c5b7784c1e85cb784913786a98c4e89110042f89
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-12 23:19:01 +0100

    update flownet5_v011.py

--- pytorch/models/flownet5_v011.py
+++ pytorch/models/flownet5_v011.py
@@ -842,10 +842,10 @@ class Model:
     def freeze(net = None):
         for param in net.block0.parameters():
             param.requires_grad = False
-        for param in net.block0.lastconv.parameters():
-            param.requires_grad = True
-        for param in net.block0.mix4.parameters():
-            param.requires_grad = True
+        # for param in net.block0.lastconv.parameters():
+        #    param.requires_grad = True
+        # for param in net.block0.mix4.parameters():
+        #    param.requires_grad = True
         for param in net.block0.convblock_last_shallow[0].mlp.parameters():
             param.requires_grad = True
         for param in net.block0.convblock_last_shallow[1].mlp.parameters():
