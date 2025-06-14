538f74241370d68324825aaf4eb8047963902fb5
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2025-06-14 09:26:28 +0100

    update flownet5_v011.py

--- pytorch/models/flownet5_v011.py
+++ pytorch/models/flownet5_v011.py
@@ -553,10 +553,10 @@ class Model:
 
                 # Sigmoid-based schedule
                 midpoint = 40000.0
-                steepness = 0.00011
+                steepness = 0.000011
                 counter_f = self.forward_counter.float()
-                # self.mix_ratio = torch.sigmoid(steepness * (counter_f - midpoint))
-                self.mix_ratio = torch.sigmoid(counter_f - midpoint)
+                self.mix_ratio = torch.sigmoid(steepness * (counter_f - midpoint))
+                # self.mix_ratio = torch.sigmoid(counter_f - midpoint)
 
                 feat = self.conv00(x)
 
